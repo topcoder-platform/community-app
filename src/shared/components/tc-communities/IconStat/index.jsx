2113d38f75ce6dbdb346c0e37dfe04aec9849b19
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2017-07-26 16:46:45 +0200

    Small correction of the previous commit
    Fixes #109

--- src/shared/components/tc-communities/IconStat/index.jsx
+++ src/shared/components/tc-communities/IconStat/index.jsx
@@ -14,9 +14,9 @@ function IconStat(props) {
   return (
     <div className={theme.container}>
       {
-        Icon && _.isString(Icon) ?
+        Icon && (_.isString(Icon) ?
           <img alt="Icon" src={Icon} className={theme.icon} /> :
-          <Icon className={theme.icon} />
+          <Icon className={theme.icon} />)
       }
       <div className={theme.text}>
         <p className={theme.number}>{number}</p>
