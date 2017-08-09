4a89d34ca7418afeedf2b319f30baeebaf0eda6b
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2017-08-09 16:55:43 +0200

    Minor update of TopGear's Home page
    Also ensures that other communities use their own copies of images for
    their Home pages.

--- src/shared/components/tc-communities/communities/qa/Home/index.jsx
+++ src/shared/components/tc-communities/communities/qa/Home/index.jsx
@@ -54,7 +54,7 @@ export default function Home(props) {
               title: 'Start Learning',
               url: 'learn',
             }}
-            imageSrc="/themes/wipro/home/image-text-learn.jpg"
+            imageSrc="/themes/qa/home/image-text-learn.jpg"
           />
           <ImageText
             title="Get Involved"
@@ -63,7 +63,7 @@ export default function Home(props) {
               title: 'Start Contributing',
               url: 'challenges',
             }}
-            imageSrc="/themes/wipro/home/image-text-do.jpg"
+            imageSrc="/themes/qa/home/image-text-do.jpg"
           />
         </div>
       </Section>
