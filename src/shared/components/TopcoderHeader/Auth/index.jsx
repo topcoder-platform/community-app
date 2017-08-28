43547b97712b96f0be654937b8898410d68121cf
Harlan <luoxwen@gmail.com>
2017-08-28 12:02:27 +0800

    Fix issue #346

--- src/shared/components/TopcoderHeader/Auth/index.jsx
+++ src/shared/components/TopcoderHeader/Auth/index.jsx
@@ -20,6 +20,7 @@ export default function Auth({ column }) {
     <div
       className={column ? 'column' : ''}
       styleName="auth"
+      onClick={(event) => { event.stopPropagation(); }}
     >
       <a
         className="tc-btn-sm tc-btn-primary"
