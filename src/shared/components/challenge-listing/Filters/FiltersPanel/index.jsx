70c94e39ec6e30afa6056ef94b4e9eeb4c8bb5ef
karthikbecse <karthikbecse@live.com>
2017-08-29 12:40:34 +0530

    Issue #384
    Issue #384

--- src/shared/components/challenge-listing/Filters/FiltersPanel/index.jsx
+++ src/shared/components/challenge-listing/Filters/FiltersPanel/index.jsx
@@ -83,6 +83,7 @@ export default function FiltersPanel({
               id="community-select"
               onChange={selectCommunity}
               options={communityOps}
+              autoBlur={true}
               simpleValue
               value={selectedCommunityId}
             />
