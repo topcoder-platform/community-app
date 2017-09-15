dca265b329f005850cb0f8eb4606b7284c924d10
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2017-09-15 17:16:43 +0200

    Blockchain: Footer is further updated

--- src/shared/components/tc-communities/Footer2/index.jsx
+++ src/shared/components/tc-communities/Footer2/index.jsx
@@ -14,6 +14,37 @@ export default function Footer2() {
       <div styleName="content">
         <TopcoderLogoGray />
         <span styleName="copyright">&copy; Topcoder, 2017</span>
+        <div styleName="right">
+          <a
+            href="https://www.topcoder.com/about-topcoder/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >About</a>
+          &bull;
+          <a
+            href="https://help.topcoder.com/hc/en-us/articles/219069687-Contact-Support"
+            rel="noopener noreferrer"
+            target="_blank"
+          >Contact</a>
+          &bull;
+          <a
+            href="https://help.topcoder.com/hc/en-us"
+            rel="noopener noreferrer"
+            target="_blank"
+          >Help</a>
+          &bull;
+          <a
+            href="https://www.topcoder.com/community/how-it-works/privacy-policy/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >Privacy</a>
+          &bull;
+          <a
+            href="https://www.topcoder.com/community/how-it-works/terms/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >Terms</a>
+        </div>
       </div>
     </div>
   );
