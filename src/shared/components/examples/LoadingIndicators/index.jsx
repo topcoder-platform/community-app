61e1ddc289471e7f4449dc189c76adbf8f7adffe
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2017-09-18 18:29:40 +0200

    Updated demo page of loading indicators

--- src/shared/components/examples/LoadingIndicators/index.jsx
+++ src/shared/components/examples/LoadingIndicators/index.jsx
@@ -1,30 +1,43 @@
 import LoadingIndicator from 'components/LoadingIndicator';
 import React from 'react';
 
-import './style.scss';
+import greenRedTheme from './green-red-theme.scss';
+import yellowPinkTheme from './yellow-pink-theme.scss';
+import style from './style.scss';
 
-export default function Buttons() {
+export default function LoadingIndicators() {
   return (
-    <div styleName="page">
+    <div styleName="style.page">
       <h1>Loading Indicators</h1>
       <p>
-        Demo of loading indicators we have in the code base (at the moment just
-        a single one, and it works fine against white background only, as
-        demonstrated below).
+        Demo of loading indicators we have in the code base. At the moment we
+        have just one type, but you can flexibly tune its size / coloring using
+        SCSS:
       </p>
-      <div styleName="white-bg">
-        <LoadingIndicator />
+      <div styleName="style.white-bg">
+        <LoadingIndicator
+          /* NOTE: Theme is used here just to be able to demo all
+           * indicators in the same line. For the real use of default
+           * LoadingIndicator, it is fine to use just
+           * <LoadingIndicator /> */
+          theme={{ container: style.indicatorContainer }}
+        />
+        <LoadingIndicator theme={greenRedTheme} />
+        <LoadingIndicator theme={yellowPinkTheme} />
       </div>
-
-      <p>
-        Here is the demo of our loading indicator against non-white backgrounds.
-        Hopefully, it will be fixed soon.
-      </p>
-      <div styleName="black-bg">
-        <LoadingIndicator />
+      <div styleName="style.black-bg">
+        <LoadingIndicator
+          theme={{ container: style.indicatorContainer }}
+        />
+        <LoadingIndicator theme={greenRedTheme} />
+        <LoadingIndicator theme={yellowPinkTheme} />
       </div>
-      <div styleName="striped-bg">
-        <LoadingIndicator />
+      <div styleName="style.striped-bg">
+        <LoadingIndicator
+          theme={{ container: style.indicatorContainer }}
+        />
+        <LoadingIndicator theme={greenRedTheme} />
+        <LoadingIndicator theme={yellowPinkTheme} />
       </div>
     </div>
   );
