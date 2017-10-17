bfeda806b95fe3f15a41b0c757b22e513f524c04
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2017-10-17 17:52:41 +0200

    F2F: Member payments - UI styling
    Submission #519346 by asmn to the challenge
    http://www.topcoder.com/challenge-details/30059934/?type=develop

--- src/shared/components/sandbox/payments/Background/index.jsx
+++ src/shared/components/sandbox/payments/Background/index.jsx
@@ -7,13 +7,22 @@
 import PT from 'prop-types';
 import React from 'react';
 import TopcoderLogo from '../../../../../assets/images/logo-topcoder-mono.svg';
+import CloseIcon from '../../../../../assets/images/sandbox/payments/close.svg';
 import './style.scss';
 
 /* TODO: For now it is just a placeholder that does nothing. */
-export default function Background({ children }) {
+export default function Background({ children, onExit, escapeButton }) {
   return (
     <div styleName="background">
       <TopcoderLogo styleName="logo" />
+      {escapeButton &&
+        <a styleName="esc" onClick={onExit} role="button" tabIndex={-1}>
+          <div styleName="button">
+            <CloseIcon />
+          </div>
+          <div styleName="text">ESC</div>
+        </a>
+      }
       {children}
     </div>
   );
@@ -21,8 +30,12 @@ export default function Background({ children }) {
 
 Background.defaultProps = {
   children: null,
+  escapeButton: true,
+  onExit: () => {},
 };
 
 Background.propTypes = {
   children: PT.node,
+  onExit: PT.func,
+  escapeButton: PT.bool,
 };
--- src/shared/components/sandbox/payments/Editor/index.jsx
+++ src/shared/components/sandbox/payments/Editor/index.jsx
@@ -6,30 +6,61 @@ import React from 'react';
 import Select from 'components/Select';
 import { PrimaryButton } from 'components/buttons';
 
+import Avatar from '../../../Avatar';
 import Background from '../Background';
 
 import './style.scss';
+
 
 export default function Editor() {
   return (
     <Background>
       <div styleName="container">
         <h1 styleName="title">New Member Payment</h1>
-        Project
-        <Select />
-        Billing account
-        <Select />
-        Title
-        <Select />
-        Description
-        <Select />
-        Assign to
-        <Select />
-        Amount
-        <Select />
-        <PrimaryButton
-          to="/sandbox/payments/123/done"
-        >Pay now</PrimaryButton>
+        <div styleName="form">
+          <div styleName="field">
+            <span styleName="label">Project</span>
+            <Select autoBlur />
+          </div>
+          <div styleName="field">
+            <span styleName="label">Billing account</span>
+            <Select autoBlur />
+          </div>
+          <div styleName="fieldGap" />
+          <div styleName="field">
+            <span styleName="label">Title</span>
+            <input placeholder="Topcoder payment" />
+          </div>
+          <div styleName="field">
+            <span styleName="label">Description</span>
+            <textarea rows={3} placeholder="payment is for ..." />
+          </div>
+          <div styleName="field">
+            <span styleName="label">Assign to</span>
+            <div styleName="withPrefix">
+              <div styleName="prefix">
+                <div styleName="avatarPrefix" >
+                  <Avatar />
+                </div>
+              </div>
+              <input placeholder="Type handle to assign member" />
+            </div>
+          </div>
+          <div styleName="field">
+            <span styleName="label">Amount</span>
+            <div styleName="withPrefix">
+              <div styleName="prefix">
+                <div styleName="textPrefix">$</div>
+              </div>
+              <input placeholder="0" />
+            </div>
+          </div>
+          <div styleName="action">
+            <PrimaryButton
+              to="/sandbox/payments/123/done"
+            >Pay now</PrimaryButton>
+          </div>
+        </div>
       </div>
     </Background>
   );
