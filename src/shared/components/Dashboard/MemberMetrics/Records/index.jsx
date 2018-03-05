7157c2e541d93e6f271c824e690c59edb0d1c01e
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-03-05 01:12:22 +0100

    Additional fixes of responsiveness
    a

--- src/shared/components/Dashboard/MemberMetrics/Records/index.jsx
+++ src/shared/components/Dashboard/MemberMetrics/Records/index.jsx
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import Carousel from 'nuka-carousel';
+import Carousel from 'components/Carousel';
 import PT from 'prop-types';
 import React from 'react';
 
@@ -8,8 +8,6 @@ import LArrow from '../../../../../assets/images/arrow-prev.svg';
 import RArrow from '../../../../../assets/images/arrow-next.svg';
 
 import './style.scss';
-
-/* global window */
 
 /**
  * Transforms stats object to a more convenient array representation.
@@ -63,99 +61,29 @@ function transformStats(stats) {
   return res;
 }
 
-export default class Records extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.check = this.check.bind(this);
-    this.width = 0;
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.check);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.check);
-  }
-
-  check() {
-    if (!this.carousel) return;
-    const st = this.carousel.state;
-    const showLeft = st.left < 0;
-    const showRight = st.currentSlide < st.slideCount - st.slidesToScroll;
-    if (showLeft === this.state.showLeft
-    && showRight === this.state.showRight) return;
-    this.setState({ showLeft, showRight });
-  }
-
-  render() {
-    const st = this.state;
-    const { stats } = this.props;
-
-    const decorators = [];
-    if (st.showLeft) {
-      decorators.push({
-        component: () => (
-          <LArrow
-            onClick={() => this.carousel && this.carousel.previousSlide()}
-          />
-        ),
-        position: 'CenterLeft',
-        style: {
-          cursor: 'pointer',
-          marginLeft: 40,
-        },
-      });
-    }
-    if (st.showRight) {
-      decorators.push({
-        component: () => (
-          <RArrow
-            onClick={() => this.carousel && this.carousel.nextSlide()}
-          />
-        ),
-        position: 'CenterRight',
-        style: {
-          cursor: 'pointer',
-          marginRight: 40,
-        },
-      });
-    }
-
-    return (
-      <div>
-        <Carousel
-          afterSlide={() => setImmediate(() => this.check())}
-          decorators={decorators}
-          framePadding="0 100px"
-          ref={(node) => {
-            this.carousel = node;
-            if (node) this.check();
-          }}
-          slidesToScroll="auto"
-          slideWidth="200px"
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          {
-            transformStats(stats).map(item => (
-              <Dial
-                key={`${item.track}-${item.subTrack}`}
-                handle={stats.handle}
-                track={item.track}
-                subTrack={item.subTrack}
-                metric={item.metric}
-                value={item.value}
-              />
-            ))
-          }
-        </Carousel>
-      </div>
-    );
-  }
+export default function Records({ stats }) {
+  return (
+    <div>
+      <Carousel
+        NextButton={RArrow}
+        PrevButton={LArrow}
+        slideWidth="200px"
+      >
+        {
+          transformStats(stats).map(item => (
+            <Dial
+              key={`${item.track}-${item.subTrack}`}
+              handle={stats.handle}
+              track={item.track}
+              subTrack={item.subTrack}
+              metric={item.metric}
+              value={item.value}
+            />
+          ))
+        }
+      </Carousel>
+    </div>
+  );
 }
 
 Records.propTypes = {
