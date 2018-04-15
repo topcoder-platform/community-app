920426777223753d462f8a6374edb60d9ab9b8dc
Adam Lowe <a.lowe@telus.net>
2018-04-14 18:10:47 -0600

    Implements Profile Skills list.

---
+++ src/shared/components/ProfilePage/Skill/index.jsx
@@ -0,0 +1,37 @@
+/**
+ * Skill Component.  Displays an icon and label dynamically based on data.
+ */
+import React from 'react';
+import PT from 'prop-types';
+import { truncate } from 'lodash';
+
+import { isClientSide } from 'utils/isomorphy';
+
+import FallbackIcon from 'assets/images/profile/skills/id-develop.svg';
+
+import './styles.scss';
+
+let assets;
+
+if (isClientSide()) {
+  assets = require.context('assets/images/profile/skills', false, /svg/);
+}
+
+const Skill = ({
+  tagId,
+  tagName,
+}) => (
+  <div styleName="container">
+    <div styleName="skill-icon">
+      { assets && assets.keys().includes(`./id-${tagId}.svg`) ? <img src={assets(`./id-${tagId}.svg`)} alt="Skill Icon" /> : <FallbackIcon /> }
+    </div>
+    <div styleName="name">{truncate(tagName, 20)}</div>
+  </div>
+);
+
+Skill.propTypes = {
+  tagId: PT.string.isRequired,
+  tagName: PT.string.isRequired,
+};
+
+export default Skill;
