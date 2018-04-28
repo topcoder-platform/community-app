0c6348499768584f28b270a2dc5069a0f258a825
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-04-28 06:27:52 +0200

    Adds FAQ at BSIC Incubator page in Blockchain community

--- src/shared/components/tc-communities/communities/blockchain/BsicHackathon/index.jsx
+++ src/shared/components/tc-communities/communities/blockchain/BsicHackathon/index.jsx
@@ -1,4 +1,5 @@
 import JoinCommunity from 'containers/tc-communities/JoinCommunity';
+import PT from 'prop-types';
 import React from 'react';
 import { Button } from 'topcoder-react-ui-kit';
 import { Link } from 'topcoder-react-utils';
@@ -16,10 +17,14 @@ import ChallengeImgRefugees from
   'assets/images/communities/blockchain/bsic-hackathon/challenge-refugees.jpg';
 
 import ChallengeCard from './ChallengeCard';
+import FaqItem from './FaqItem';
 
 import style from './style.scss';
 
-export default function BsicHackathon() {
+export default function BsicHackathon({
+  shownFaqItems,
+  toggleFaqItem,
+}) {
   return (
     <div>
       <div styleName="bannerContainer">
@@ -128,6 +133,331 @@ export default function BsicHackathon() {
             </li>
           </ul>
         </div>
+      </div>
+      <div styleName="faq">
+        <h2>FAQ for Competitors</h2>
+        <FaqItem
+          open={shownFaqItems.n01}
+          question="What do I absolutely need to know about the #ImpactIncubator?"
+          toggle={show => toggleFaqItem('n01', show)}
+        >
+          <ul>
+            <li>
+              There are four challenges: #Hardware, #Democracy,
+              #Agriculture, #Refugees
+            </li>
+            <li>
+              The required deliverables are (1) Weekly checkpoints
+              &zwnj;<a
+                href="https://paper.dropbox.com/doc/Weekly-checkpoints-09FvI6B1wAuT7qqVvw7kz?_tk=share_copylink"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://paper.dropbox.com/doc/Weekly-checkpoints-09FvI6B1wAuT7qqVvw7kz?_tk=share_copylink
+              </a> and (2) a 5-min video demo
+            </li>
+            <li>
+              You will be scored by such criteria
+              &zwnj;<a
+                href="https://airtable.com/shryJsiCL6nc2hieb"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://airtable.com/shryJsiCL6nc2hieb
+              </a>
+            </li>
+            <li>
+              Every team has 3 mentors. There’s a post in your private
+              forum telling you who they are
+              &zwnj;<a
+                href="https://airtable.com/shrCUgy4ZgOSCoveO"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://airtable.com/shrCUgy4ZgOSCoveO
+              </a>
+            </li>
+            <li>
+              All submissions can be changed, modified, and replaced until
+              May 21, when your final submission is due and the Incubator
+              ends
+            </li>
+          </ul>
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n02}
+          question="How do I join the Incubator Slack?"
+          toggle={show => toggleFaqItem('n02', show)}
+        >
+          Click this link to join the slack domain:
+          &zwnj;<a
+            href="https://apps.topcoder.com/forums/?module=Thread&threadID=914879&start=0&mc=1#2254786"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://apps.topcoder.com/forums/?module=Thread&threadID=914879&start=0&mc=1#2254786
+          </a>
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n03}
+          question="Can I join multiple challenges or multiple teams?"
+          toggle={show => toggleFaqItem('n03', show)}
+        >
+          No. You can only register for one sub-challenge and join only one
+          team. If you are currently on multiple teams, please contact
+          @sophieqgu to choose one team to move forward. If you’ve not made
+          a choice, you will be disqualified as a prize recipient.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n04}
+          question="Can I add team members after registration? Can I change team members after registration??"
+          toggle={show => toggleFaqItem('n04', show)}
+        >
+          No. You may not change team members after the registration
+          deadline.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n05}
+          question="How do I see the forums? How do I see my Team Private Forum?"
+          toggle={show => toggleFaqItem('n05', show)}
+        >
+          Make sure you’re registered on the main challenge and your use
+          case challenge. When you go to the use case challenge page, you
+          should see a link to forums.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n06}
+          question="I have problems accessing the forums. What do I do?"
+          toggle={show => toggleFaqItem('n06', show)}
+        >
+          If you aren’t registered on the sub-challenge, or you haven’t
+          been added to a private forum, reach out to the copilot
+          (@birdofpreyru) on the BSIC Slack.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n07}
+          question="What does my team do if mentors don’t show?"
+          toggle={show => toggleFaqItem('n07', show)}
+        >
+          Please let us know and we will get you a new mentor(s) asap!
+          Contact @sophieqgu in Slack
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n08}
+          question="What if I don’t want to use the forums?"
+          toggle={show => toggleFaqItem('n08', show)}
+        >
+          We have provided the forums as a means to hold files with context,
+          meaning you post in the forum and upload a file for your
+          team/mentor. You are more than welcome to coordinate with your
+          team and mentors to work another way (eg: Google Drive Folder,
+          Dropbox, Git(hub/lab).
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n09}
+          question="What must be in the forum?"
+          toggle={show => toggleFaqItem('n09', show)}
+        >
+          We really want questions about the Incubator in general in the
+          main challenge forum if possible. This gives visibility to the
+          answers of your questions to the whole list of participants to
+          keep things fair. It also helps us not answer the same question
+          100 times, once for each team. If your question is of a more
+          sensitive or specific nature, the question should be asked of
+          your mentor in your private forum.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n10}
+          question="What counts as a usable submission?"
+          toggle={show => toggleFaqItem('n10', show)}
+        >
+          A submission that addresses the Weekly questions! It is an ideal
+          scenario that you address all of the questions there. But if you
+          are blocked by one, just move on! Prioritize your tasks and
+          coordinate with your team to complete them.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n11}
+          question="How do I drop out of the competition?"
+          toggle={show => toggleFaqItem('n11', show)}
+        >
+          If you do not submit your final submission, then we assume
+          you’ve dropped out.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n13}
+          question="How do we deal with in-team disputes?"
+          toggle={show => toggleFaqItem('n13', show)}
+        >
+          We understand the risks of working with strangers, but we trust
+          you to solve your differences first among yourselves. You are
+          welcome to flag those issues to us to help us understand your
+          situation. While we are not able to change your contract or prize
+          split, those issues will be taken into consideration when we
+          provide post-Incubator support.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n14}
+          question="I’m having trouble uploading a project. What do I do?"
+          toggle={show => toggleFaqItem('n14', show)}
+        >
+          You are welcome to use other tools as long as it is not your
+          final submission. Please inform your mentors when you do so. If
+          it is your final submission, please contact @jcori.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n15}
+          question="Why can only the team captain upload a submission?"
+          toggle={show => toggleFaqItem('n15', show)}
+        >
+          The Topcoder platform is designed for single-user competitor
+          submissions. The team captain must be the Topcoder handle who
+          submits so we can correctly line up the submission with the
+          contract to work out the payments with the agreed-upon split.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n16}
+          question="What should my final submission contain?"
+          toggle={show => toggleFaqItem('n16', show)}
+        >
+          <p>
+            We will post a detailed list on the challenge specs, the
+            blockchain.topcoder.com/bsic-incubator website, post in the
+            forum, and cross-post in slack, but here’s a short list.
+          </p>
+          <p>A zip file containing:</p>
+          <ul>
+            <li>
+              A README.md explaining your submission .zip
+            </li>
+            <li>
+              A folder containing all your checkpoint submissions
+            </li>
+            <li>
+              Source code
+            </li>
+            <li>
+              Supporting Materials
+            </li>
+          </ul>
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n17}
+          question="What should my final submission not contain?"
+          toggle={show => toggleFaqItem('n17', show)}
+        >
+          <ul>
+            <li>
+              It should not contain your team name, names, email addresses.
+              Judging must be anonymous. We will link your submission to
+              your team via your team captain’s Topcoder handle. The judges
+              will just see your submission as a submission number.
+            </li>
+            <li>
+              It should not contain anything you would leave out in a
+              .gitignore - ie: node_modules folders, uncompressed data
+              files, etc
+            </li>
+            <li>
+              It should not contain a video file. Any video submissions
+              should be uploaded to Youtube as an unlisted video, and the
+              link provided (either in your README.md, or in a presentation
+              file)
+            </li>
+            <li>
+              It should not contain links out to Google/Dropbox. This is a
+              challenge with a deadline, everything that is submitted must
+              be “final”. If you submit a link to Google Drive, we won’t
+              know if you’ve updated the files after the deadline.
+            </li>
+          </ul>
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n18}
+          question="What if I already submitted to the challenge for my use case?"
+          toggle={show => toggleFaqItem('n18', show)}
+        >
+          When the submission phase closes, the judges will only receive
+          the latest .zip file uploaded through the platform. If you
+          uploaded before, it will just be overwritten.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n19}
+          question="How will payments be distributed?"
+          toggle={show => toggleFaqItem('n19', show)}
+        >
+          Participants must have a valid Topcoder handle. The prizes will
+          be awarded through the Topcoder platform to the handles based on
+          the split agreed upon in the Docusign contract.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n20}
+          question="Who owns the IP of my submissions?"
+          toggle={show => toggleFaqItem('n20', show)}
+        >
+          Your team only. BSIC, Topcoder, and ConsenSys waive all claims on
+          IP.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n21}
+          question="When will I know the results?"
+          toggle={show => toggleFaqItem('n21', show)}
+        >
+          The review process will begin as soon as the Incubator ends on
+          May 21. We will notify the winners individually towards the end
+          of May. All teams&apos; scores will be made public after our
+          conference on June 1.
+        </FaqItem>
+        <br />
+        <h3>FAQ for Mentors</h3>
+        <FaqItem
+          open={shownFaqItems.n22}
+          question="How many teams are there?"
+          toggle={show => toggleFaqItem('n22', show)}
+        >
+          We have 94 so far. Here’s a team list:
+          &zwnj;<a
+            href="https://airtable.com/shrrtV5S2261uBCNJ"
+          >
+            https://airtable.com/shrrtV5S2261uBCNJ
+          </a>
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n23}
+          question="What happens if I cannot reach my team?"
+          toggle={show => toggleFaqItem('n23', show)}
+        >
+          Please let us know. We will try to contact them to make sure they
+          know about the rules.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n24}
+          question="What teams are assigned to me?"
+          toggle={show => toggleFaqItem('n24', show)}
+        >
+          You should’ve got an email listing 2-3 teams that you are
+          assigned to. If you have not got an email, please get in touch
+          socialimpact@consensys.net. If you cannot get access to the
+          forums we provided in the email, please contact @jcori.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n25}
+          question="Can I see submissions from all teams or just my own teams?"
+          toggle={show => toggleFaqItem('n25', show)}
+        >
+          You can see submissions from only your own teams.
+        </FaqItem>
+        <FaqItem
+          open={shownFaqItems.n26}
+          question="What happens if the team delays in submitting?"
+          toggle={show => toggleFaqItem('n26', show)}
+        >
+          If teams submitted all weekly checkpoints timely, they will get
+          the portion of the Delivery scores. However, the weekly
+          checkpoints will not be immediately evaluated after each
+          deadline. You can work with them on those deliverables until the
+          Incubator ends on May 21.
+        </FaqItem>
       </div>
       <div styleName="challengesContainer">
         <div styleName="challengesContent">
@@ -365,3 +695,9 @@ export default function BsicHackathon() {
     </div>
   );
 }
+
+
+BsicHackathon.propTypes = {
+  shownFaqItems: PT.shape().isRequired,
+  toggleFaqItem: PT.func.isRequired,
+};
