e3bcd50920b6feae6722f972729e835f432265ef
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-05-09 02:52:43 +0200

    Fix of dashboard announcement's "max text width" setting behaviour
    Fix for #839

--- src/shared/components/Dashboard/Announcement/index.jsx
+++ src/shared/components/Dashboard/Announcement/index.jsx
@@ -82,12 +82,7 @@ export default function Announcement({
         }}
       >
         { preview ? <h1 styleName="previewLabel">Preview</h1> : null }
-        <div
-          style={{
-            maxWidth: maxTextWidth,
-          }}
-          styleName="details"
-        >
+        <div styleName="details">
           <div
             onClick={() => switchShow(false)}
             role="button"
@@ -97,11 +92,17 @@ export default function Announcement({
           { type ? <div styleName="type">{type}</div> : null }
           <h1
             styleName="title"
-            style={{ color: fontColor }}
+            style={{
+              color: fontColor,
+              maxWidth: maxTextWidth,
+            }}
           >{publicTitle || title}</h1>
           <div
             styleName="text"
-            style={{ color: fontColor }}
+            style={{
+              color: fontColor,
+              maxWidth: maxTextWidth,
+            }}
           >{text}</div>
           {
             readMore ? (
