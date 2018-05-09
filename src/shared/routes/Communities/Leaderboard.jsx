488c2dd3634f06c17156548675ec3db6cbf406a3
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-05-09 02:44:56 +0200

    Update of banners in TopGear
    Fix for #835

--- src/shared/routes/Communities/Leaderboard.jsx
+++ src/shared/routes/Communities/Leaderboard.jsx
@@ -7,7 +7,10 @@ import PT from 'prop-types';
 import React from 'react';
 import { SplitRoute } from 'utils/router';
 
-export default function LeaderboardRoute({ meta }) {
+export default function LeaderboardRoute({
+  HeadBanner,
+  meta,
+}) {
   return (
     <SplitRoute
       cacheCss
@@ -17,7 +20,10 @@ export default function LeaderboardRoute({ meta }) {
           /* webpackChunkName: "leaderboard/chunk" */
           'containers/Leaderboard',
         ).then(({ default: Leaderboard }) => (
-          <Leaderboard apiUrl={meta.leaderboardApiUrl} />
+          <Leaderboard
+            apiUrl={meta.leaderboardApiUrl}
+            HeadBanner={HeadBanner}
+          />
         ))
       }
       renderPlaceholder={() => <LoadingIndicator />}
@@ -25,7 +31,12 @@ export default function LeaderboardRoute({ meta }) {
   );
 }
 
+LeaderboardRoute.defaultProps = {
+  HeadBanner: null,
+};
+
 LeaderboardRoute.propTypes = {
+  HeadBanner: PT.func,
   meta: PT.shape({
     leaderboardApiUrl: PT.string.isRequired,
   }).isRequired,
