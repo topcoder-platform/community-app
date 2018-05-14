9a8b7bb047be6f682e92166a844859b8fcdd4cab
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-05-14 14:22:18 +0200

    Contentful banners are wired to TopGear pages

---
+++ src/shared/components/tc-communities/communities/wipro/LeaderboardBanner.jsx
@@ -0,0 +1,17 @@
+import Banner from 'components/Contentful/Banner';
+import ContentfulLoader from 'containers/ContentfulLoader';
+import LoadingIndicator from 'components/LoadingIndicator';
+import React from 'react';
+
+export default function ChallengeListingBanner() {
+  return (
+    <ContentfulLoader
+      entryQueries={{
+        content_type: 'banner',
+        'fields.name': 'TopGear - Leaderboard - Banner',
+      }}
+      render={d => d.entries.matches[0].items.map(id => <Banner id={id} />)}
+      renderPlaceholder={LoadingIndicator}
+    />
+  );
+}
