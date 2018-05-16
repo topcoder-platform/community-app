fe6cf6fefc2296623acd04cf35b0cb5d403e237a
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-05-17 00:01:22 +0200

    Fixes challenge type icons in the challenge listing

--- src/shared/components/TrackIcon/index.jsx
+++ src/shared/components/TrackIcon/index.jsx
@@ -2,6 +2,7 @@ import config from 'utils/config';
 import React from 'react';
 import PT from 'prop-types';
 import Abbreviation from './Abbreviation';
+
 import './style.scss';
 
 export default function TrackIcon({
@@ -12,9 +13,10 @@ export default function TrackIcon({
   MAIN_URL,
 }) {
   const TCO_URL = `${MAIN_URL}/tco`;
+  const st = subTrack.toUpperCase().replace(/ /g, '_');
   return (
     <span styleName="trackIcon">
-      <div styleName={`${(isDataScience ? 'data_science' : track.toLowerCase())} main-icon`}>{Abbreviation[track][subTrack]}</div>
+      <div styleName={`${(isDataScience ? 'data_science' : track.toLowerCase())} main-icon`}>{Abbreviation[track][st]}</div>
       <a href={`${TCO_URL}`}>
         <div styleName={tcoEligible ? `${(isDataScience ? 'data_science' : track.toLowerCase())} tco-icon` : 'hidden'}>TCO</div>
       </a>
--- src/shared/components/challenge-listing/Tooltips/TrackAbbreviationTooltip/index.jsx
+++ src/shared/components/challenge-listing/Tooltips/TrackAbbreviationTooltip/index.jsx
@@ -14,7 +14,7 @@ import './style.scss';
 const DESCRIPTION = {
   // TODO: [Review Opps] This is temporary
   REVIEW_OPPORTUNITY: 'As a topcoder member, you can participate in review boards that review work submitted into challenges by other members.',
-  APPLICATION_FRONT_END_DESIGN: 'Design UI and front end experiences for apps',
+  'APPLICATION_FRONT-END_DESIGN': 'Design UI and front end experiences for apps',
   ARCHITECTURE: 'Architect modules, components, or full applications',
   ASSEMBLY_COMPETITION: 'Develop code for a variety of use cases. Rigorous review and final fix process is included.',
   BANNERS_OR_ICONS: 'Design UI assets for use in web, mobile, print, and other digital formats',
@@ -22,21 +22,21 @@ const DESCRIPTION = {
   CODE: 'Develop code for apps, services, etc. Final fixes are not included',
   CONCEPTUALIZATION: 'Discover and define user stories and requirements',
   DESIGN_FIRST_2_FINISH: 'Be the first to deliver the design solution',
-  FIRST_2_FINISH: 'Be the first to deliver the development solution',
+  FIRST2FINISH: 'Be the first to deliver the development solution',
   LOGO_DESIGN: 'Logo Design',
   MARATHON_MATCH: 'Write algorythms to solve complex problems, often for real world issues',
-  PRINT_OR_PRESENTATION: 'Design print and presentation assets',
+  'PRINT/PRESENTATION': 'Design print and presentation assets',
   SRM: 'Single Round Match - quickly write code to solve algorythm problems head to head against other competitors',
   STUDIO_OTHER: 'Studio other',
   UI_PROTOTYPE_COMPETITION: 'Develop the front end of a UX',
-  WEB_DESIGNS: 'Design UI and front end experiences for web experiences',
+  WEB_DESIGN: 'Design UI and front end experiences for web experiences',
   WIDGET_OR_MOBILE_SCREEN_DESIGN: 'Design UI and front end experiences for mobile',
   WIREFRAMES: 'Produce the information architecture for user experiences',
 };
 
 const HEADER = {
   REVIEW_OPPORTUNITY: 'Review Opportunity', // TODO: [Review Opps] this is temporary
-  APPLICATION_FRONT_END_DESIGN: 'Application Front-End Design (AFED)',
+  'APPLICATION_FRONT-END_DESIGN': 'Application Front-End Design (AFED)',
   ARCHITECTURE: 'Architecture (Ar)',
   ASSEMBLY_COMPETITION: 'Assembly (As)',
   BANNERS_OR_ICONS: 'Banners/Icons (BI)',
@@ -44,13 +44,13 @@ const HEADER = {
   CODE: 'Code (Cd)',
   CONCEPTUALIZATION: 'Conceptualization (Cn)',
   DESIGN_FIRST_2_FINISH: 'Design First2Finish(DF2F)',
-  FIRST_2_FINISH: 'First2Finish (F2F)',
+  FIRST2FINISH: 'First2Finish (F2F)',
   LOGO_DESIGN: 'Logo Design (Lg)',
   MARATHON_MATCH: 'Marathon Match',
-  PRINT_OR_PRESENTATION: 'Print/Presentation (PP)',
+  'PRINT/PRESENTATION': 'Print/Presentation (PP)',
   SRM: 'Single Round Match (SRM)',
   UI_PROTOTYPE_COMPETITION: 'UI Prototype (Pr)',
-  WEB_DESIGNS: 'Web Design (Wb)',
+  WEB_DESIGN: 'Web Design (Wb)',
   WIDGET_OR_MOBILE_SCREEN_DESIGN: 'Widget or Mobile Screen Design (Wg)',
   STUDIO_OTHER: 'Generic design challenge (D)',
   WIREFRAMES: 'Wireframe (Wf)',
@@ -90,7 +90,8 @@ function placeArrow(TooltipNode) {
  * Renders the tooltip.
  */
 function TrackAbbreviationTooltip(props) {
-  const tip = <Tip track={props.track} subTrack={props.subTrack} />;
+  const subTrack = props.subTrack.toUpperCase().replace(/ /g, '_');
+  const tip = <Tip track={props.track} subTrack={subTrack} />;
   return (
     <Tooltip
       className="track-abbreviation-tooltip"
