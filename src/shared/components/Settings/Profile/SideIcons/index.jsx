8a1d39e027e13e18b75d21f0fa4563266187de49
Henry <2087393593@qq.com>
2018-06-18 21:50:04 +0800

    update profile-seeting basic info

---
+++ src/shared/components/Settings/Profile/SideIcons/index.jsx
@@ -0,0 +1,23 @@
+/**
+ * export a icons objct
+ * used by side bar component
+ */
+import basicInfo from 'assets/images/profile/sideicons/basicinfo.png';
+import language from 'assets/images/profile/sideicons/language.png';
+import education from 'assets/images/profile/sideicons/education.png';
+import work from 'assets/images/profile/sideicons/work.png';
+import organization from 'assets/images/profile/sideicons/organization.png';
+import skill from 'assets/images/profile/sideicons/skill.png';
+import hobby from 'assets/images/profile/sideicons/hobby.png';
+import community from 'assets/images/profile/sideicons/community.png';
+
+export default {
+  'basic info': basicInfo,
+  language,
+  education,
+  work,
+  organization,
+  skill,
+  hobby,
+  community,
+};
