e93cdfd6fb6fc73c61a69511ae9f986e486f398a
Kiril Kartunov <kiril.kartunov@gmail.com>
2018-06-19 07:42:46 +0300

    Mobile Community link new pages

--- src/shared/routes/Communities/Mobile/Routes.jsx
+++ src/shared/routes/Communities/Mobile/Routes.jsx
@@ -2,12 +2,12 @@
  * Routing of Mobile Community.
  */
 
-/* TODO: This assembly of custom challenge listing page should be split out into
- * a separate component. But, it is good enough for now. */
+import ChallengeListing from 'routes/Communities/ChallengeListing';
 import Error404 from 'components/Error404';
 import Footer from 'components/tc-communities/Footer2';
 import Header from 'containers/tc-communities/Header';
 import Home from 'containers/tc-communities/mobile/Home';
+import Learn from 'containers/tc-communities/mobile/Learn';
 import PT from 'prop-types';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
@@ -17,7 +17,7 @@ import { config, MetaTags } from 'topcoder-react-utils';
 import primaryButtonStyle from 'components/buttons/outline/round/open-sans/green-uppercase.scss';
 import secondaryButtonStyle from 'components/buttons/outline/round/open-sans/blue-uppercase.scss';
 
-export default function Mobile({ base }) {
+export default function Mobile({ base, meta }) {
   return (
     <Route
       component={({ match }) => (
@@ -50,6 +50,27 @@ export default function Mobile({ base }) {
                 path={`${base}`}
               />
               <Route
+                component={Learn}
+                exact
+                path={`${base}/learn`}
+              />
+              <Route
+                component={() => (
+                  <div>
+                    {
+                      ChallengeListing({
+                        challengesUrl: `${base}/challenges`,
+                        meta,
+                        listingOnly: true,
+                        newChallengeDetails: true,
+                      })
+                   }
+                  </div>
+                )}
+                exact
+                path={`${base}/challenges`}
+              />
+              <Route
                 component={Error404}
                 path={`${base}/:any`}
               />
@@ -65,8 +86,10 @@ export default function Mobile({ base }) {
 
 Mobile.defaultProps = {
   base: '',
+  meta: {},
 };
 
 Mobile.propTypes = {
   base: PT.string,
+  meta: PT.shape({}),
 };
