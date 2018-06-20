37632f1d9e58e1efa5bd6a604e5636539b8b7be4
Kiril Kartunov <kiril.kartunov@gmail.com>
2018-06-20 21:28:26 +0300

    IoT linking menu pages

--- src/shared/components/tc-communities/communities/iot/About/index.jsx
+++ src/shared/components/tc-communities/communities/iot/About/index.jsx
@@ -1,55 +1,10 @@
-/**
- * About static page of IoT community
- */
-/* eslint-disable max-len */
 import React from 'react';
-import PT from 'prop-types';
+import Viewport from 'components/Contentful/Viewport';
 
-import predixLogoSrc from 'assets/themes/iot/about/predix-logo.png';
-import geLogoSrc from 'assets/themes/iot/about/ge-digital-logo.png';
-import geniuslinkLogoSrc from 'assets/themes/iot/about/geniuslink-logo.png';
-
-import Section from '../../../Section';
-import JoinSection from '../JoinSection';
-import TopBanner from './TopBanner';
-
-import styles from './styles.scss';
-
-export default function About({
-  baseUrl,
-}) {
+export default function Home() {
   return (
-    <main styleName="main">
-      <TopBanner />
-      <Section
-        theme={{
-          container: styles.quoteContainer,
-        }}
-      >
-        <div styleName="logos">
-          <a href="http://predix.io/" rel="noopener noreferrer" target="_blank">
-            <img src={predixLogoSrc} alt="Predix logo" />
-          </a>
-          <a href="http://predix.io/" rel="noopener noreferrer" target="_blank">
-            <img src={geLogoSrc} alt="GE Digital logo" />
-          </a>
-          <a href="http://predix.io/" rel="noopener noreferrer" target="_blank">
-            <img src={geniuslinkLogoSrc} alt="GENIUSLINK logo" />
-          </a>
-        </div>
-        <div styleName="quote">
-          <h1>“Industrial challenges require the global real-time big data analytics provided by the GE Predix platform.”</h1>
-          <h3>Mark Wright</h3>
-          <p>President of digital commerce solutions, Pitney Bowes</p>
-        </div>
-      </Section>
-      <JoinSection
-        baseUrl={baseUrl}
-      />
-    </main>
+    <Viewport
+      query={{ 'fields.name': 'IoT Community - About' }}
+    />
   );
 }
-
-About.propTypes = {
-  baseUrl: PT.string.isRequired,
-};
--- src/shared/components/tc-communities/communities/iot/Home/index.jsx
+++ src/shared/components/tc-communities/communities/iot/Home/index.jsx
@@ -1,86 +1,10 @@
-/**
- * Home static page of IoT community
- */
-/* eslint-disable max-len */
-
 import React from 'react';
-import PT from 'prop-types';
+import Viewport from 'components/Contentful/Viewport';
 
-import PredixLogoSrc from 'assets/themes/iot/home/predix.png';
-
-import YouTubeVideo from 'components/YouTubeVideo';
-import Section from '../../../Section';
-import Quote from '../../../Quote';
-import JoinSection from '../JoinSection';
-import TopBanner from './TopBanner';
-
-import styles from './styles.scss';
-
-export default function Home({
-  baseUrl,
-}) {
+export default function Home() {
   return (
-    <main styleName="main">
-      <TopBanner
-        baseUrl={baseUrl}
-      />
-      <Section
-        theme={{
-          container: styles.welcomeContainer,
-        }}
-      >
-        <div>
-          <h2>Welcome to the Topcoder Community for Predix</h2>
-          <Quote
-            authorDescription="GE Digital, Predix Developer Relations"
-            authorName="Lothar Schubert"
-            authorPhotoURL="http://predix.topcoder.com/wp-content/uploads/sites/7/2016/10/lothar.png"
-          >
-            “Predix is GE’s platform for the Industrial Internet of Things (IIoT). I’d like to invite you to join over 20,000 developers and data scientists around the world, working on the forefront of the digital-industrial revolution.”
-          </Quote>
-        </div>
-      </Section>
-      <Section
-        theme={{
-          container: styles.predixCloudContainer,
-        }}
-      >
-        <div>
-          <img src={PredixLogoSrc} alt="Predix" />
-          <h1>Your cloud platform for the Industrial Internet</h1>
-          <h3>Built on Cloud Foundry, Predix is optimized for secure connectivity<br /> and analytics at scale – in the Cloud and on the Edge</h3>
-        </div>
-      </Section>
-      <YouTubeVideo
-        src="https://www.youtube.com/embed/HU2xr_wjR3s"
-        rel={false}
-        showinfo={false}
-        videoId="HU2xr_wjR3s"
-        thumb={<div styleName="videoPlayButton" />}
-      />
-      <Section
-        theme={{
-          container: styles.singleQuoteContainer,
-        }}
-      >
-        <Quote
-          authorDescription="Software & Services GM, GE Digital."
-          theme={{
-            text: styles.singleQuoteText,
-          }}
-        >
-          “We can’t do everything ourselves, nor should we. There are things that we need to be unapologetically awesome at, and there are areas where we need to partner, <br />
-          GE wants to be <strong>unapologetically awesome</strong> at data and analytics”<br />
-          says Ashley Haynes-Gaspar,
-        </Quote>
-      </Section>
-      <JoinSection
-        baseUrl={baseUrl}
-      />
-    </main>
+    <Viewport
+      query={{ 'fields.name': 'IoT Community - Home' }}
+    />
   );
 }
-
-Home.propTypes = {
-  baseUrl: PT.string.isRequired,
-};
