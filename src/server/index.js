5f95070e15e73bd0b0969f8e2c186cd7f5944c72
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-07-07 14:35:57 +0200

    Fix: 200 success status when accessing non-existing assets
    It caused random problems with CDN caching during re-deployments of the
    app.

--- src/server/index.js
+++ src/server/index.js
@@ -56,6 +56,7 @@ const EXTRA_SCRIPTS = [
 ];
 
 const MODE = process.env.BABEL_ENV;
+const webpackConfig = webpackConfigFactory(MODE);
 
 async function beforeRender(req, suggestedConfig) {
   const [
@@ -172,10 +173,20 @@ async function onExpressJsSetup(server) {
    * for static assets. */
   const url = path.resolve(__dirname, '../../build');
   server.use('/community-app-assets', express.static(url));
+
+  /* If a build asset has not been found till this point, we fallthrough
+   * a simple error response, around the standard ReactJS rendering. Otherwise,
+   * it is too easy to mess with the reponse status, which can cause caching
+   * problems in CDN during re-deployments of the app. */
+  server.use(webpackConfig.output.publicPath, (req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+  });
 }
 
 global.KEEP_BUILD_INFO = true;
-serverFactory(webpackConfigFactory(MODE), {
+serverFactory(webpackConfig, {
   Application,
   beforeRender,
   devMode: MODE === 'development',
