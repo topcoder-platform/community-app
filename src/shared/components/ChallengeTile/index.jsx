3888c383275d83e529b2a1a1edd70e5bac452ee0
Xiang Mi <mixiangimm@gmail.com>
2018-07-07 10:38:05 +0800

    Member Statistics - Challenges and SRMs

---
+++ src/shared/components/ChallengeTile/index.jsx
@@ -0,0 +1,235 @@
+/**
+ * Challenge tile.
+ */
+/* eslint-env browser */
+import _ from 'lodash';
+import React from 'react';
+import PT from 'prop-types';
+import { Link } from 'react-router-dom';
+import { formatDate } from 'utils/tc';
+import ReactImageFallback from 'react-image-fallback';
+import InviteOnly from 'assets/images/ico-invite-only-prj.svg';
+import WinnerRibbon from 'assets/images/ico-winner-ribbon.svg';
+import NoImage from 'assets/images/card-bg-no-image.png';
+import Private from 'assets/images/ico-private-prj.svg';
+import Gallery from 'assets/images/ico-gallery.svg';
+import './style.scss';
+
+const underscoreReplace = (string) => {
+  const map = {
+    ASSEMBLY_COMPETITION: 'ASSEMBLY',
+  };
+  if (map[string]) {
+    return map[string];
+  }
+  if (!string) {
+    return '';
+  }
+  return string.replace(/_/g, ' ');
+};
+
+const listRoles = (roles) => {
+  if (!roles) {
+    return 'No assigned role.';
+  }
+
+  const rolesString = roles.join(', ');
+
+  if (rolesString.length > 60) {
+    return `${rolesString.slice(0, 57)}...`;
+  }
+
+  return rolesString;
+};
+
+const percentage = x => `${Math.round(x * 100)}%`;
+
+const getPlacementPostfix = (x) => {
+  if (x === 1) {
+    return 'st';
+  } else if (x === 2) {
+    return 'nd';
+  } else if (x === 3) {
+    return 'rd';
+  }
+  return 'th';
+};
+
+class ChallengeTile extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.onPopModal = this.onPopModal.bind(this);
+  }
+
+  onPopModal() {
+    const { onPopModal, index, challenge } = this.props;
+    if (!challenge.isPrivate && challenge.imageURL && onPopModal) {
+      onPopModal(index);
+    }
+  }
+
+  render() {
+    const {
+      challenge,
+    } = this.props;
+
+    const outStyleName = `challenge tile-view ${challenge.track}`;
+    const extraStyle = {
+      width: '285px',
+      padding: '15px',
+      margin: '10px 5px',
+    };
+
+    const roundId = challenge.track === 'DATA_SCIENCE' ? _.get(challenge, 'rounds.0.id') : 0;
+
+    return (
+      <div styleName="challenge tile" style={extraStyle}>
+        <div styleName={outStyleName}>
+          <div styleName="completed-challenge">
+            <header>
+              { challenge.track !== 'DATA_SCIENCE' && (!challenge.isPrivate ?
+                <Link to={`/challenges/${challenge.id}`} styleName="name">
+                  <span>{ challenge.name }</span>
+                </Link> : <span>{ challenge.name }</span>) }
+
+              { challenge.track === 'DATA_SCIENCE' &&
+                <a styleName="name" href={`https://community.topcoder.com/longcontest/stats/?module=ViewOverview&rd=${roundId}`}>
+                  { challenge.name }
+                </a> }
+
+              <p styleName="subtrack-color">{underscoreReplace(challenge.subTrack)}</p>
+              <p styleName="date-completed">{formatDate(challenge.submissionEndDate)}</p>
+              { challenge.wonFirst && !challenge.isPrivate &&
+              <div styleName="winner-ribbon" >
+                <WinnerRibbon />
+              </div>
+                    }
+            </header>
+            <div styleName="challenge-card__bottom">
+              <div styleName="challenge-details DATA_SCIENCE">
+                { challenge.track === 'DATA_SCIENCE' && challenge.subTrack &&
+                  <div styleName="marathon-score">
+                    <p styleName="score">{ challenge.pointTotal || 0 }</p>
+                    <p>Total Points</p>
+                  </div> }
+                { challenge.track === 'DEVELOP' &&
+                  <div styleName="dev-challenge-user-place">
+                    <div styleName="tile-view">
+                      { challenge.highestPlacement &&
+                        <p styleName={challenge.wonFirst ? 'first-place place' : 'place'}>
+                          { challenge.highestPlacement }
+                          <span>{ getPlacementPostfix(challenge.highestPlacement) }</span> Place
+                        </p>
+                      }
+                      { challenge.userStatus === 'NOT_FINISHED' &&
+                        <p styleName="place">Didn&apos;t Finish</p> }
+                      { challenge.userStatus === 'PASSED_SCREENING' &&
+                        <p styleName="place">Passed Screening</p> }
+                      { !challenge.highestPlacement && challenge.userStatus === 'PASSED_REVIEW' &&
+                        <p styleName="place">Passed Review</p> }
+                      { challenge.userStatus === 'COMPLETED' &&
+                        <p styleName="place">COMPLETED</p> }
+                      { challenge.userStatus === 'PASSED_REVIEW' &&
+                        <div styleName="challenge-score">
+                          <p styleName="score">{ percentage(challenge.userDetails.submissionReviewScore / 100) }</p>
+                          <p styleName="last-child">Review Score</p>
+                        </div> }
+
+                    </div>
+                  </div>
+                }
+                {
+                  challenge.track === 'DESIGN' && !challenge.isPrivate &&
+                    <div styleName="design-challenge-user-place">
+                      <div styleName="tile-view">
+                        { challenge.highestPlacement &&
+                        <p styleName={challenge.wonFirst ? 'first-place place' : 'place'}>
+                          { challenge.highestPlacement }
+                          <span>{ getPlacementPostfix(challenge.highestPlacement) }</span> Place
+                        </p>
+                        }
+                        { challenge.userStatus === 'NOT_FINISHED' &&
+                          <p styleName="place">Didn&apos;t Finish</p> }
+                        { challenge.userStatus === 'PASSED_SCREENING' &&
+                          <p styleName="place">Passed Screening</p> }
+                        { !challenge.highestPlacement && challenge.userStatus === 'PASSED_REVIEW' &&
+                          <p styleName="place">Passed Review</p> }
+                        { challenge.userStatus === 'COMPLETED' &&
+                          <p styleName="place">COMPLETED</p> }
+                        {
+                          challenge.userStatus === 'PASSED_REVIEW' &&
+                            <div styleName="thumbnail" onClick={this.onPopModal} role="presentation">
+                              {challenge.submissionViewable && challenge.imageURL &&
+                              <ReactImageFallback
+                                initialImage={NoImage}
+                                src={challenge.imageURL}
+                                fallbackImage={NoImage}
+                              />}
+                              {challenge.submissionViewable && !challenge.imageURL &&
+                              <ReactImageFallback
+                                src={NoImage}
+                              />}
+                              {!challenge.submissionViewable &&
+                                <div styleName="private-challenge-banner" title="Submissions for this challenge are confidential">
+                                  <Private />
+                                  <span>PRIVATE CHALLENGE</span>
+                                </div>
+                              }
+                              {challenge.numImages > 0 &&
+                                <div styleName="thumbnail-gallery">
+                                  <div styleName="gallery-icon" >
+                                    <Gallery />
+                                  </div>
+                                  <div styleName="num-images">
+                                    {challenge.submissionViewable &&
+                                      <span>{challenge.numImages} IMAGES</span>
+                                    }
+                                    {!challenge.submissionViewable &&
+                                      <span>No image is visible</span>
+                                    }
+                                  </div>
+                                </div>
+                              }
+                            </div>
+                        }
+                        {challenge.userStatus !== 'PASSED_REVIEW' && <div styleName="thumbnail" />}
+                      </div>
+                    </div>
+                }
+                { challenge.isPrivate &&
+                <div styleName="invite-only-banner">
+                  <div styleName="title">INVITE-ONLY CHALLENGE</div>
+                  <InviteOnly />
+                  <span>CHALLENGE INFORMATION IS CONFIDENTIAL<br />
+                  RESULTS ARE NOT INCLUDED IN STATISTICS
+                  </span>
+                </div> }
+              </div>
+
+              <p styleName="roles">
+                { challenge.track !== 'DATA_SCIENCE' &&
+                  <span>
+                    <span>Role: &nbsp;</span>
+                    <span>{ listRoles(challenge.userDetails.roles) }</span>
+                  </span> }
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ChallengeTile.defaultProps = {
+  onPopModal: null,
+  index: -1,
+};
+
+ChallengeTile.propTypes = {
+  challenge: PT.shape().isRequired,
+  onPopModal: PT.func,
+  index: PT.number,
+};
+
+export default ChallengeTile;
---
+++ src/shared/components/ProfilePage/Stats/SubTrackChallengeView/index.jsx
@@ -0,0 +1,481 @@
+/**
+ * challenge page.  Displays challenges
+ * of a subtrack.
+ */
+/* eslint-env browser */
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import PT from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroller';
+import shortId from 'shortid';
+
+import ChallengeTile from 'components/ChallengeTile';
+import SRMTile from 'components/SRMTile';
+import { actions } from 'topcoder-react-lib';
+import LoadingIndicator from 'components/LoadingIndicator';
+import GalleryModal from './GalleryModal';
+import './style.scss';
+
+// how many challenges to query per batch
+const CHALLENGE_PER_PAGE = 36;
+
+/**
+ * @static
+ * @desc pre-process subtrach challenge array to prepare for rendering
+ * @param {String} one raw challenge just retrieved from API
+ */
+const processPastChallenge = (challenge) => {
+  const cloned = _.cloneDeep(challenge);
+  if (cloned.userDetails) {
+    // process placement for challenges having winningPlacements array in response
+    if (Array.isArray(cloned.userDetails.winningPlacements)) {
+      cloned.highestPlacement = _.min(cloned.userDetails.winningPlacements, 'placed').placed;
+    }
+    // process placement for design challenges
+    if (cloned.track === 'DESIGN' &&
+        cloned.userDetails.submissions &&
+        cloned.userDetails.submissions.length > 0) {
+      cloned.thumbnailId = cloned.userDetails.submissions[0].id;
+    }
+    // determines the user status for passing the review for one of the submissions
+    if (cloned.userDetails.submissions && cloned.userDetails.submissions.length > 0) {
+      cloned.passedReview = cloned.userDetails.submissions.filter(submission =>
+        submission.type === 'Contest Submission'
+        && (submission.status === 'Active'
+          || submission.status === 'Completed Without Win')).length > 0;
+    }
+    if (cloned.highestPlacement === 0) {
+      cloned.highestPlacement = null;
+    }
+    cloned.wonFirst = cloned.highestPlacement === 1;
+
+    cloned.userHasSubmitterRole = false;
+
+    // determines if user has submitter role or not
+    const { roles } = cloned.userDetails;
+    if (roles.length > 0) {
+      const submitterRole = _.findIndex(roles, (role) => {
+        const lRole = role.toLowerCase();
+        return lRole === 'submitter';
+      });
+      if (submitterRole >= 0) {
+        cloned.userHasSubmitterRole = true;
+      }
+    }
+
+    if (!cloned.userStatus) {
+      if (cloned.userDetails.hasUserSubmittedForReview) {
+        if (!cloned.passedReview) {
+          cloned.userStatus = 'PASSED_SCREENING';
+        } else {
+          cloned.userStatus = 'PASSED_REVIEW';
+        }
+      } else {
+        cloned.userStatus = 'NOT_FINISHED';
+      }
+
+      // if user does not has submitter role, just show Completed
+      if (!cloned.userHasSubmitterRole) {
+        cloned.userStatus = 'COMPLETED';
+      }
+    }
+
+    // adjust submissions
+    if (cloned.userDetails &&
+        cloned.userDetails.submissions &&
+        cloned.userDetails.submissions.length > 0) {
+      cloned.userDetails.submissions = cloned.userDetails.submissions.filter(submission =>
+        submission && submission.submissionImage);
+      cloned.userDetails.submissions = _.sortBy(
+        cloned.userDetails.submissions,
+        submission => submission.placement,
+      );
+    }
+
+    if (!cloned.isPrivate &&
+        cloned.userDetails &&
+        cloned.userDetails.submissions &&
+        cloned.userDetails.submissions.length > 0) {
+      // add attr imageURL
+      cloned.imageURL = cloned.userDetails.submissions[0].submissionImage &&
+                           cloned.userDetails.submissions[0].submissionImage.full;
+      // add numImages
+      cloned.numImages = cloned.userDetails.submissions.length;
+    } else {
+      cloned.numImages = 0;
+    }
+  }
+  return cloned;
+};
+
+class SubTrackChallengeView extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    // this is current page number. starts with 0.
+    // everytime we scroll at the bottom, we query from offset = pageNum * CHALLENGE_PER_PAGE
+    this.state = {
+      // this is current page number. starts with 0.
+      // everytime we scroll at the bottom, we query from offset = pageNum * CHALLENGE_PER_PAGE
+      pageNum: 0,
+      // which challenge's modal should be poped. null means no modal
+      challengeIndexToPopModal: null,
+    };
+    this.onPageChange = this.onPageChange.bind(this);
+    this.onPopModal = this.onPopModal.bind(this);
+    this.onCloseModal = this.onCloseModal.bind(this);
+  }
+
+  componentDidMount() {
+    const {
+      handle,
+      auth,
+      track,
+      subTrack,
+      loadingSubTrackChallengesUUID,
+      loadSubtrackChallenges,
+      loadingSRMUUID,
+      loadSRM,
+      loadingMarathonUUID,
+      loadMarathon,
+    } = this.props;
+
+    if (track === 'DEVELOP' || track === 'DESIGN') {
+      if (!loadingSubTrackChallengesUUID) {
+        loadSubtrackChallenges(handle, auth.tokenV3, track, subTrack, 0, CHALLENGE_PER_PAGE, true);
+      }
+    } else if (track === 'DATA_SCIENCE') {
+      if (subTrack === 'SRM') {
+        if (!loadingSRMUUID) {
+          loadSRM(handle, auth.tokenV3, 0, CHALLENGE_PER_PAGE, true);
+        }
+      } else if (subTrack === 'MARATHON_MATCH') {
+        if (!loadingMarathonUUID) {
+          loadMarathon(handle, auth.tokenV3, 0, CHALLENGE_PER_PAGE, true);
+        }
+      }
+    }
+  }
+
+  onPageChange() {
+    const {
+      handle,
+      auth,
+      track,
+      subTrack,
+      loadingSubTrackChallengesUUID,
+      loadSubtrackChallenges,
+      loadingSRMUUID,
+      loadSRM,
+      loadingMarathonUUID,
+      loadMarathon,
+    } = this.props;
+
+    const {
+      pageNum,
+    } = this.state;
+
+    if ((track === 'DEVELOP' || track === 'DESIGN')) {
+      if (!loadingSubTrackChallengesUUID) {
+        loadSubtrackChallenges(
+          handle,
+          auth.tokenV3,
+          track,
+          subTrack,
+          pageNum + 1,
+          CHALLENGE_PER_PAGE,
+          false,
+        );
+        this.setState({ pageNum: pageNum + 1 });
+      }
+      // if loadingSubTrackChallengesUUID is set in redux-store,
+      // it means there is already an ongoing query
+      // same to SRM and Marathon
+    } else if (track === 'DATA_SCIENCE') {
+      if (subTrack === 'SRM') {
+        if (!loadingSRMUUID) {
+          loadSRM(handle, auth.tokenV3, pageNum + 1, CHALLENGE_PER_PAGE, false);
+          this.setState({ pageNum: pageNum + 1 });
+        }
+      } else if (subTrack === 'MARATHON_MATCH') {
+        if (!loadingMarathonUUID) {
+          loadMarathon(handle, auth.tokenV3, pageNum + 1, CHALLENGE_PER_PAGE, false);
+          this.setState({ pageNum: pageNum + 1 });
+        }
+      }
+    }
+  }
+
+  onPopModal(index) {
+    this.setState({ challengeIndexToPopModal: index });
+  }
+
+  onCloseModal() {
+    this.setState({ challengeIndexToPopModal: null });
+  }
+
+  render() {
+    const {
+      pageNum,
+      challengeIndexToPopModal,
+    } = this.state;
+
+    const {
+      track,
+      subTrack,
+      loadingSubTrackChallengesUUID,
+      loadingSRMUUID,
+      loadingMarathonUUID,
+      challenges,
+      challengesHasMore,
+      userSrms,
+      userSrmHasMore,
+      userMarathons,
+      userMarathonHasMore,
+      userId,
+    } = this.props;
+
+    if (pageNum === 0 &&
+        (loadingSubTrackChallengesUUID || loadingSRMUUID || loadingMarathonUUID)) {
+      return <LoadingIndicator />;
+    }
+
+    if (
+      ((track === 'DEVELOP' || track === 'DESIGN') && (!challenges || challenges.length === 0)) ||
+      ((track === 'DATA_SCIENCE' && subTrack === 'SRM') && (!userSrms || userSrms.length === 0)) ||
+      ((track === 'DATA_SCIENCE' && subTrack === 'MARATHON_MATCH') &&
+       (!userMarathons || userMarathons.length === 0))
+    ) {
+      return (
+        <div styleName={track}>
+          <section>
+            <div styleName="challenges">
+              <div styleName="no-challenges">Sorry, no successful challenges found.</div>
+            </div>
+          </section>
+        </div>
+      );
+    }
+
+    if (track === 'DEVELOP' || track === 'DESIGN') {
+      const challengeToRender = _.map(challenges, processPastChallenge);
+
+      return (
+        <div>
+          {challengeIndexToPopModal != null &&
+          <div>
+            <GalleryModal
+              onCancel={this.onCloseModal}
+              challenge={challengeToRender[challengeIndexToPopModal]}
+            />
+          </div>
+            }
+          <InfiniteScroll
+            initialLoad={false}
+            loadMore={this.onPageChange}
+            hasMore={!loadingSubTrackChallengesUUID && challengesHasMore}
+            loader={<LoadingIndicator key="challenge-loader" />}
+            threshold={500}
+          >
+            <div styleName={track}>
+              <section>
+                <div styleName="challenges">
+                  {challengeToRender.map((item, index) => (
+                    <ChallengeTile
+                      challenge={item}
+                      onPopModal={this.onPopModal}
+                      index={index}
+                      key={`scroll-subtrack-challenge-${item.name}`}
+                    />))}
+                </div>
+              </section>
+            </div>
+          </InfiniteScroll>
+          {loadingSubTrackChallengesUUID && <LoadingIndicator />}
+        </div>
+      );
+    } else if (track === 'DATA_SCIENCE' && subTrack === 'SRM') {
+      userSrms.sort((a, b) => {
+        const aHasFP = a.rounds[0];
+        const bHasFP = b.rounds[0];
+        if (
+          (a.rounds[0].userSRMDetails &&
+           a.rounds[0].userSRMDetails.finalPoints)
+            &&
+          (b.rounds[0].userSRMDetails &&
+           b.rounds[0].userSRMDetails.finalPoints)
+        ) {
+          // sort descending
+          return b.rounds[0].userSRMDetails.finalPoints - a.rounds[0].userSRMDetails.finalPoints;
+        } else if (bHasFP) {
+          // if b has FP, b should go first
+          return 1;
+        } else if (aHasFP) {
+          return -1;
+        }
+        return 0;
+      });
+
+      return (
+        <div>
+          <InfiniteScroll
+            initialLoad={false}
+            loadMore={this.onPageChange}
+            hasMore={!loadingSRMUUID && userSrmHasMore}
+            loader={<LoadingIndicator key="srm-loader" />}
+            threshold={500}
+          >
+            <div styleName={track}>
+              <section>
+                <div styleName="challenges">
+                  {userSrms.map(item =>
+                    <SRMTile key={`srm-${item.name}`} challenge={item} userId={userId} />)
+                  }
+                </div>
+              </section>
+            </div>
+          </InfiniteScroll>
+          {loadingSRMUUID && <LoadingIndicator />}
+        </div>
+      );
+    } else if (track === 'DATA_SCIENCE' && subTrack === 'MARATHON_MATCH') {
+      const marathonToRender = _.map(
+        userMarathons,
+        item => ({
+          ...item,
+          submissionEndDate: _.get(item, 'rounds.0.systemTestEndAt'),
+          pointTotal: _.get(item, 'rounds.0.userMMDetails.pointTotal'),
+        }),
+      );
+      return (
+        <div>
+          <InfiniteScroll
+            initialLoad={false}
+            loadMore={this.onPageChange}
+            hasMore={!loadingMarathonUUID && userMarathonHasMore}
+            loader={<LoadingIndicator key="marathon-loader" />}
+            threshold={500}
+          >
+            <div styleName={track}>
+              <section>
+                <div styleName="challenges">
+                  {marathonToRender.map(item =>
+                    <ChallengeTile key={`marathon-${item.name}`} challenge={item} />)
+                  }
+                </div>
+              </section>
+            </div>
+          </InfiniteScroll>
+          {loadingMarathonUUID && <LoadingIndicator />}
+        </div>
+      );
+    }
+
+    return null;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  handle: ownProps.handle,
+  auth: state.auth,
+  track: ownProps.track,
+  subTrack: ownProps.subTrack,
+
+  challenges: state.members[ownProps.handle] ?
+    state.members[ownProps.handle].subtrackChallenges : null,
+  challengesHasMore: state.members[ownProps.handle] ?
+    state.members[ownProps.handle].subtrackChallengesHasMore : null,
+  loadingSubTrackChallengesUUID: state.members[ownProps.handle] ?
+    state.members[ownProps.handle].loadingSubTrackChallengesUUID : null,
+
+  loadingSRMUUID: state.members[ownProps.handle] ?
+    state.members[ownProps.handle].loadingSRMUUID : null,
+  userSrms: state.members[ownProps.handle] ? state.members[ownProps.handle].userSRMs : null,
+  userSrmHasMore: state.members[ownProps.handle] ?
+    state.members[ownProps.handle].userSRMHasMore : null,
+
+  loadingMarathonUUID: state.members[ownProps.handle] ?
+    state.members[ownProps.handle].loadingMarathonUUID : null,
+  userMarathons: state.members[ownProps.handle] ?
+    state.members[ownProps.handle].userMarathons : null,
+  userMarathonHasMore: state.members[ownProps.handle] ?
+    state.members[ownProps.handle].userMarathonHasMore : null,
+
+  userId: ownProps.userId,
+});
+
+function mapDispatchToProps(dispatch) {
+  const action = actions.members;
+
+  return {
+    loadSubtrackChallenges: (handle, tokenV3, track, subTrack, pageNum, pageSize, refresh) => {
+      const uuid = shortId();
+      dispatch(action.getSubtrackChallengesInit(handle, uuid));
+      dispatch(action.getSubtrackChallengesDone(
+        uuid,
+        handle,
+        tokenV3,
+        track,
+        subTrack,
+        pageNum,
+        pageSize,
+        refresh,
+      ));
+    },
+    loadSRM: (handle, tokenV3, pageNum, pageSize, refresh) => {
+      const uuid = shortId();
+      dispatch(action.getUserSrmInit(handle, uuid));
+      dispatch(action.getUserSrmDone(uuid, handle, tokenV3, pageNum, pageSize, refresh));
+    },
+    loadMarathon: (handle, tokenV3, pageNum, pageSize, refresh) => {
+      const uuid = shortId();
+      dispatch(action.getUserMarathonInit(handle, uuid));
+      dispatch(action.getUserMarathonDone(uuid, handle, tokenV3, pageNum, pageSize, refresh));
+    },
+  };
+}
+
+SubTrackChallengeView.defaultProps = {
+  challenges: null,
+  challengesHasMore: null,
+  loadingSubTrackChallengesUUID: null,
+
+  userSrms: null,
+  loadingSRMUUID: null,
+  userSrmHasMore: null,
+
+  loadingMarathonUUID: null,
+  userMarathons: null,
+  userMarathonHasMore: null,
+};
+
+SubTrackChallengeView.propTypes = {
+  handle: PT.string.isRequired,
+  auth: PT.shape().isRequired,
+  track: PT.string.isRequired,
+  subTrack: PT.string.isRequired,
+  userId: PT.number.isRequired,
+
+  challenges: PT.arrayOf(PT.shape()),
+  challengesHasMore: PT.bool,
+  loadingSubTrackChallengesUUID: PT.string,
+
+  userSrms: PT.arrayOf(PT.shape()),
+  loadingSRMUUID: PT.string,
+  userSrmHasMore: PT.bool,
+
+  loadingMarathonUUID: PT.string,
+  userMarathons: PT.arrayOf(PT.shape()),
+  userMarathonHasMore: PT.bool,
+
+  loadSubtrackChallenges: PT.func.isRequired,
+  loadSRM: PT.func.isRequired,
+  loadMarathon: PT.func.isRequired,
+};
+
+const Container = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SubTrackChallengeView);
+
+export default Container;
