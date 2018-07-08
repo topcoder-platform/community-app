66eaef228766c92398eff955a914609c045ef4fd
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-07-09 00:12:44 +0200

    Another update of markdown tests

--- __tests__/shared/utils/markdown.js
+++ __tests__/shared/utils/markdown.js
@@ -1,27 +1,25 @@
 import fs from 'fs';
 import render from 'utils/markdown';
+import { Stringify } from 'postcss';
 
-test('Generic Markdown Test #1', () => {
-  const url = `${__dirname}/__mocks__/markdown-test-01.md`;
+/**
+ * Performs snapshot test of markdown rendering, using the specified file as
+ * the test markup.
+ * @param {Stringify} filename
+ */
+function check(filename) {
+  const url = `${__dirname}/__mocks__/${filename}`;
   const doc = fs.readFileSync(url).toString();
   expect(render(doc)).toMatchSnapshot();
-});
+}
 
-test('Generic Markdown Test #2', () => {
-  const url = `${__dirname}/__mocks__/markdown-test-02.md`;
-  const doc = fs.readFileSync(url).toString();
-  expect(render(doc)).toMatchSnapshot();
-});
+test('Generic Markdown Test #1', () => check('markdown-test-01.md'));
+test('Generic Markdown Test #2', () => check('markdown-test-02.md'));
 
-test('Custom Inline Elements (via simplified JSX)', () => {
-  const url = `${__dirname}/__mocks__/markdown-test-03.md`;
-  const doc = fs.readFileSync(url).toString();
-  expect(render(doc)).toMatchSnapshot();
-});
+test('Custom Inline Elements (via simplified JSX)',
+  () => check('markdown-test-03.md'));
 
-test('Generic HTML markup inside Markdown code', () => {
-  const url = `${__dirname}/__mocks__/markdown-test-04.md`;
-  const doc = fs.readFileSync(url).toString();
-  expect(render(doc)).toMatchSnapshot();
-});
+test('Generic HTML markup inside Markdown code',
+  () => check('markdown-test-04.md'));
 
+test('Test of code blocks', () => check('markdown-test-05.md'));
