e343037ff1f6a93725019aa757efe6a1591c1978
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-07-11 03:52:23 +0200

    Contentful-related fixes / updates for Nick
    Fix for #1116
    Fix for #1118
    Duplicates track homepages at /community/{TRACK_NAME} route

--- src/shared/routes/Topcoder/Routes.jsx
+++ src/shared/routes/Topcoder/Routes.jsx
@@ -23,6 +23,7 @@ import Settings from '../Settings';
 import HallOfFame from '../HallOfFame';
 import Profile from '../Profile';
 import Scoreboard from '../tco/scoreboard';
+import TrackHomePages from '../TrackHomePages';
 import ProfileStats from '../ProfileStats';
 import './styles.scss';
 
@@ -80,6 +81,10 @@ export default function Topcoder() {
               component={ProfileStats}
               exact
               path="/members/:handle([\w\-\[\].{}]{2,15})/details"
+            />
+            <Route
+              component={() => <TrackHomePages base="/track" />}
+              path="/community"
             />
             <Error404 />
           </Switch>
