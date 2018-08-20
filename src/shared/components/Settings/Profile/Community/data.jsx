7ac480f9edb13c94f7739b2f5ee1e647b848b010
nqviet <nqvietit@gmail.com>
2018-08-21 05:10:05 +0700

    fixed issue #1230

--- src/shared/components/Settings/Profile/Community/data.jsx
+++ src/shared/components/Settings/Profile/Community/data.jsx
@@ -18,20 +18,6 @@ const data = [
     name: 'Topcoder Cognitive Community',
     description: 'By becoming a member of the Topcoder Community and registering for this specialized community, you can compete in fun cognitive challenges, access educational resources, and win money by solving real-life business problems for companies in need of cognitive expertise. ',
   },
-  {
-    id: 'ios',
-    icon: ios,
-    programID: 3445,
-    name: 'IOS Community',
-    description: 'You’re eager to get started, and we have a bunch of iOS/Swift challenges lined up to give you new opportunities to earn cash. We also have an exclusive iOS badging program to prove your app design and development expertise, so dive in and start competing!',
-  },
-  {
-    id: 'predix',
-    icon: predix,
-    programID: 3448,
-    name: 'Topcoder Community for Predix',
-    description: 'With this program, you will become well-versed in the Predix platform and have a better understanding of the Industrial Internet and the Industrial Internet of Things. You will also be well ahead of the curve and ready to put your Predix skills to use and help drive the future of industry.',
-  },
 ];
 
 export default data;
