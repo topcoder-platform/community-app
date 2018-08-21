35fddf3446c45ed9e6f254097ae5ec95fdcbe719
Kevin Crain <kevin.c@superbalist.com>
2018-08-21 19:42:11 +0200

    Only display user consent modal if user consent is undefined

--- src/shared/components/Settings/ConsentComponent/index.jsx
+++ src/shared/components/Settings/ConsentComponent/index.jsx
@@ -6,6 +6,7 @@
 /* eslint-disable jsx-a11y/label-has-for */
 import React from 'react';
 import UserConsentModal from 'components/Settings/UserConsentModal';
+import PT from 'prop-types';
 
 export default class ConsentComponent extends React.Component {
   constructor(props) {
@@ -14,6 +15,8 @@ export default class ConsentComponent extends React.Component {
     this.shouldRenderConsent = this.shouldRenderConsent.bind(this);
     this.renderConsent = this.renderConsent.bind(this);
     this.showConsent = this.showConsent.bind(this);
+    ConsentComponent.getTraitData = ConsentComponent.getTraitData.bind(this);
+    ConsentComponent.getUserConsent = ConsentComponent.getUserConsent.bind(this);
 
     this.state = {
       onConsent: undefined,
@@ -30,6 +33,39 @@ export default class ConsentComponent extends React.Component {
     onConsent(answer);
   }
 
+  static getTraitData(userTraits) {
+    const trait = userTraits.filter(t => t.traitId === 'personalization');
+    if (trait.length !== 0) {
+      return trait[0].traits.data[0];
+    }
+    return null;
+  }
+
+  static getUserConsent(userTraits) {
+    const traitData = ConsentComponent.getTraitData(userTraits);
+
+    if (traitData && typeof traitData.userConsent === 'boolean') {
+      return traitData.userConsent;
+    }
+    // by default, when personalization user trait hasn't been
+    // created yet, user doesn't give consent
+    return false;
+  }
+
+  /**
+   * Show User Consent Modal
+   * @param onConsent callback after consent
+   */
+  showConsent(onConsent) {
+    const { userTraits } = this.props;
+    const userConsent = ConsentComponent.getUserConsent(userTraits);
+    if (userConsent === undefined) {
+      this.setState({ onConsent });
+    } else {
+      onConsent(userConsent);
+    }
+  }
+
   /**
    * Determines if view show render consent
    *
@@ -40,15 +76,11 @@ export default class ConsentComponent extends React.Component {
     return onConsent;
   }
 
-  /**
-   * Show User Consent Modal
-   * @param onConsent callback after consent
-   */
-  showConsent(onConsent) {
-    this.setState({ onConsent });
-  }
-
   renderConsent() {
     return (<UserConsentModal onSaveTrait={this.onHandleConsent} />);
   }
 }
+
+ConsentComponent.propTypes = {
+  userTraits: PT.array.isRequired,
+};
