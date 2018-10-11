07aed04e3e649c6d32a15c5838620ffb0b8b072b
Nahid Mahbub <nahidshahin@yahoo.com>
2018-10-11 21:41:52 +0600

    issues:1685: Changed style of New password field

--- src/shared/components/Settings/Account/MyAccount/index.jsx
+++ src/shared/components/Settings/Account/MyAccount/index.jsx
@@ -585,7 +585,7 @@ export default class MyAccount extends React.Component {
                           </div>
                         </div>
                         <div styleName="row">
-                          <div styleName="field col-1 password">
+                          <div styleName="field col-1">
                             <label htmlFor="new-password">
                                 New password
                             </label>
