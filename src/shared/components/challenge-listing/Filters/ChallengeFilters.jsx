f2b091569b9ebe53cd9dea433dcf85f03d4c876e
gurmeetb <gurmeetb>
2018-12-17 19:41:12 +0530

    issue #1816 - log filterstate

--- src/shared/components/challenge-listing/Filters/ChallengeFilters.jsx
+++ src/shared/components/challenge-listing/Filters/ChallengeFilters.jsx
@@ -52,9 +52,9 @@ export default function ChallengeFilters({
   if (filterState.endDate || filterState.startDate) filterRulesCount += 1;
 
   const isTrackOn = (track) => {
-    console.log('checking track[' + track + '] in ' + JSON.stringify(filterState));
+    console.log(`checking track[${track}] in ${JSON.stringify(filterState)}`);
     const isEnabled = filterState.tracks ? Boolean(filterState.tracks[track]) : true;
-    console.log('Found track[' + track + ']: ' + isEnabled);
+    console.log(`Found track[${track}] in ${isEnabled}`);
     return isEnabled;
   };
 
