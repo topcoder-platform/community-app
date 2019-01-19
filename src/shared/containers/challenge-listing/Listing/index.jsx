506d115471e755ed2dec2fc3b60cabd2982e3c49
Sushil Shinde <shinde.sushil@gmail.com>
2019-01-19 10:42:55 +0530

    Revert "Fix issue with fetching communities list"

--- src/shared/containers/challenge-listing/Listing/index.jsx
+++ src/shared/containers/challenge-listing/Listing/index.jsx
@@ -57,7 +57,7 @@ export class ListingContainer extends React.Component {
       selectBucket(queryBucket);
     }
 
-    if (!_.isEmpty(communitiesList.loadingUuid)
+    if (!communitiesList.loadingUuid
     && (Date.now() - communitiesList.timestamp > USER_GROUP_MAXAGE)) {
       getCommunitiesList(auth);
     }
