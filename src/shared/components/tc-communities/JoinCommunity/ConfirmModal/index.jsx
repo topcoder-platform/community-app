a3b52d806a586d3e7c19259edf3e0e63c36b63ec
Kiril Kartunov <kiril.kartunov@gmail.com>
2019-02-28 08:24:16 +0200

    Fixed texts on join community modals

--- src/shared/components/tc-communities/JoinCommunity/ConfirmModal/index.jsx
+++ src/shared/components/tc-communities/JoinCommunity/ConfirmModal/index.jsx
@@ -32,11 +32,7 @@ export default function ConfirmModal({
   let text;
   if (userId) {
     text = (
-      <p>
-Do you want to join
-        {communityName}
-?
-      </p>
+      <p>Do you want to join the&nbsp;{communityName}?</p>
     );
     if (skipConfirmJoin) {
       setImmediate(() => join(token, joinGroupId || groupIds[0], userId));
@@ -44,14 +40,8 @@ Do you want to join
   } else {
     text = customTcAuthModalText || (
       <div>
-        <p>
-You must be a Topcoder member before you can join the
-          {communityName}
-.
-        </p>
-        <p>
-To join, login if you are already a member. If not, register first.
-        </p>
+        <p>You must be a Topcoder member before you can join the&nbsp;{communityName}</p>
+        <p>To join, login if you are already a member. If not, register first.</p>
       </div>
     );
   }
