9db5d08707f9f4ec883d2e97bf486a65cbcf1ac8
applehit16 <applehit16@gmail.com>
2019-04-17 22:52:12 -0700

    fix notification not showing up for settings tabs and clear notification when change tabs

--- src/shared/components/Settings/Preferences/index.jsx
+++ src/shared/components/Settings/Preferences/index.jsx
@@ -18,6 +18,7 @@ import Referral from 'assets/images/preferences/referral.svg';
 import SideBar from 'components/Settings/SideBar';
 import ErrorWrapper from 'components/Settings/ErrorWrapper';
 import Email from './Email';
+import { SCREEN_SIZE } from '../constants';
 
 import './styles.scss';
 
@@ -40,42 +41,54 @@ const icons = {
 export default class Preferences extends React.Component {
   constructor(props) {
     super(props);
-
-    this.toggleTab = this.toggleTab.bind(this);
-    this.renderTabContent = this.renderTabContent.bind(this);
-
     this.state = {
       currentTab: 'e-mail',
+      isMobileView: false,
     };
+    this.previousTab = null;
+    this.toggleTab = this.toggleTab.bind(this);
+    this.renderTabContent = this.renderTabContent.bind(this);
+    this.clearNotifiation = this.clearNotifiation.bind(this);
+    this.updatePredicate = this.updatePredicate.bind(this);
   }
 
   componentDidMount() {
-    const {
-      clearToastrNotification,
-    } = this.props;
-    clearToastrNotification();
+    this.clearNotifiation();
+    this.updatePredicate();
+    window.addEventListener('resize', this.updatePredicate);
+  }
+
+  componentDidUpdate() {
+    const { currentTab } = this.state;
+    if (currentTab !== this.previousTab) {
+      this.clearNotifiation();
+    }
   }
 
   componentWillUnmount() {
-    const {
-      clearToastrNotification,
-    } = this.props;
-    clearToastrNotification();
+    this.clearNotifiation();
+    window.removeEventListener('resize', this.updatePredicate);
+  }
+
+  clearNotifiation() {
+    const { clearToastrNotification } = this.props;
+    if (clearToastrNotification) {
+      clearToastrNotification();
+    }
+  }
+
+  updatePredicate() {
+    this.setState({ isMobileView: window.innerWidth <= SCREEN_SIZE.SM });
   }
 
   /* Add this to resolve checkbox checked issue when switch mobile to other device */
   toggleTab(tab) {
+    const { currentTab } = this.state;
+    this.previousTab = currentTab;
     this.setState({ currentTab: tab });
   }
 
   renderTabContent(tab) {
-    const {
-      clearToastrNotification,
-    } = this.props;
-    if (this.previousSelectedTab !== tab && clearToastrNotification) {
-      clearToastrNotification();
-    }
-    this.previousSelectedTab = tab;
     switch (tab) {
       case 'e-mail':
         return <Email {...this.props} />;
@@ -95,21 +108,24 @@ export default class Preferences extends React.Component {
   render() {
     const {
       currentTab,
+      isMobileView,
     } = this.state;
 
     const names = Object.keys(tabs).map(key => tabs[key]);
 
     return (
       <div styleName="preferences-container">
-        <div styleName="mobile-view">
-          <Accordion
-            icons={icons}
-            names={names}
-            currentSidebarTab={currentTab}
-            renderTabContent={this.renderTabContent}
-            toggleSidebarTab={this.toggleTab}
-          />
-        </div>
+        {isMobileView && (
+          <div styleName="mobile-view">
+            <Accordion
+              icons={icons}
+              names={names}
+              currentSidebarTab={currentTab}
+              renderTabContent={this.renderTabContent}
+              toggleSidebarTab={this.toggleTab}
+            />
+          </div>
+        )}
         <div styleName="col-bar">
           <SideBar
             icons={icons}
@@ -118,11 +134,15 @@ export default class Preferences extends React.Component {
             toggle={this.toggleTab}
           />
         </div>
-        <div styleName="col-content">
-          <ErrorWrapper>
-            { this.renderTabContent(currentTab) }
-          </ErrorWrapper>
-        </div>
+        {
+          !isMobileView && (
+          <div styleName="col-content">
+            <ErrorWrapper>
+              { this.renderTabContent(currentTab) }
+            </ErrorWrapper>
+          </div>
+          )
+        }
       </div>
     );
   }
