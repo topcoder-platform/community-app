6f86ab410cc25d628fa456894b83e7b4e05313bb
Nahid Mahbub <nahidshahin@yahoo.com>
2019-06-14 17:00:37 +0600

    Pass openMore shared variable between TopNav and LoginNav

--- src/shared/components/Header/index.jsx
+++ src/shared/components/Header/index.jsx
@@ -18,9 +18,18 @@ try {
 
 const Header = ({ profile }) => {
   const [activeLevel1Id, setActiveLevel1Id] = useState();
+  const [openMore, setOpenMore] = useState(true);
 
   const handleChangeLevel1Id = (menuId) => {
     setActiveLevel1Id(menuId);
+  };
+
+  const handleCloseOpenMore = () => {
+    setOpenMore(false);
+  };
+
+  const handleChangeOpenMore = (openMore) => {
+    setOpenMore(openMore);
   };
 
   const handleSwitchMenu = () => {
@@ -48,6 +57,7 @@ const Header = ({ profile }) => {
               accountMenu={config.ACCOUNT_MENU}
               switchText={config.ACCOUNT_MENU_SWITCH_TEXT}
               onSwitch={handleSwitchMenu}
+              onMenuOpen={handleCloseOpenMore}
               showNotification={false}
               profile={normalizedProfile}
               authURLs={config.HEADER_AUTH_URLS}
@@ -57,6 +67,8 @@ const Header = ({ profile }) => {
           theme={config.HEADER_MENU_THEME}
           currentLevel1Id={activeLevel1Id}
           onChangeLevel1Id={handleChangeLevel1Id}
+          openMore={openMore}
+          setOpenMore={handleChangeOpenMore}
         />
       </div>
     );
