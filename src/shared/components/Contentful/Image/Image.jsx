bfe664f5582ba64fc12d5fe8ddd406f75c39bc7a
Kiril Kartunov <kiril.kartunov@gmail.com>
2019-07-20 07:14:06 +0300

    Shapes and Images for Contemtful

---
+++ src/shared/components/Contentful/Image/Image.jsx
@@ -0,0 +1,105 @@
+/**
+ * The core image rendering.
+ */
+
+/* global document */
+
+import PT from 'prop-types';
+import React from 'react';
+import _ from 'lodash';
+import { themr } from 'react-css-super-themr';
+import { fixStyle } from 'utils/contentful';
+import SVG from 'react-inlinesvg';
+
+import LoadingIndicator from 'components/LoadingIndicator';
+import defaultTheme from './themes/default.scss';
+
+export class ImageInner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.onLoadSvg = this.onLoadSvg.bind(this);
+  }
+
+  componentDidUpdate() {
+    const {
+      clipSvg,
+    } = this.props;
+    const clipSvgUrl = _.get(clipSvg, 'file.url');
+    if (clipSvgUrl) {
+      this.onLoadSvg();
+    }
+  }
+
+  /**
+   * event call after load svg success
+   */
+  onLoadSvg() {
+    const {
+      id,
+    } = this.props;
+    const imgTag = document.querySelector(`#${id} img`);
+    const svgClip = document.querySelector(`#${id} svg clipPath`);
+    if (svgClip) {
+      svgClip.id = `svg-id-${id}`;
+      imgTag.style['clip-path'] = `url(#${svgClip.id})`;
+      imgTag.style.display = 'block';
+    }
+  }
+
+  render() {
+    const {
+      theme,
+      id,
+      image,
+      imageSource,
+      clipSvg,
+    } = this.props;
+
+    const imageUrl = _.get(imageSource, 'file.url');
+    const clipSvgUrl = _.get(clipSvg, 'file.url');
+    const imgStyle = {};
+    if (clipSvgUrl) {
+      imgStyle.display = 'none';
+    }
+
+    return (
+      <div
+        id={id}
+        className={theme['img-wrap']}
+        style={fixStyle(image.extraStylesForContainer)}
+      >
+        {clipSvgUrl && (
+          <SVG
+            preloader={<LoadingIndicator />}
+            src={clipSvgUrl}
+            wrapper={React.createFactory('svg')}
+            onLoad={this.onLoadSvg}
+          />
+        )}
+        <img
+          src={imageUrl}
+          alt=""
+          style={imgStyle}
+        />
+      </div>
+    );
+  }
+}
+
+ImageInner.defaultProps = {
+  clipSvg: null,
+};
+
+ImageInner.propTypes = {
+  id: PT.string.isRequired,
+  image: PT.shape().isRequired,
+  imageSource: PT.shape().isRequired,
+  clipSvg: PT.shape(),
+  theme: PT.shape({
+    'img-wrap': PT.string,
+  }).isRequired,
+};
+
+export default themr('Image', defaultTheme)(ImageInner);
