d154da90650da5690d85b69410d9ecd50d1fbca3
Afrisal Yodi Purnama <afrisalyp@gmail.com>
2019-07-23 20:20:09 +0700

    Put Banner, Nav, Main, Contentinfo Landmark

--- src/shared/components/TopcoderHeader/index.jsx
+++ src/shared/components/TopcoderHeader/index.jsx
@@ -337,6 +337,7 @@ export default class TopcoderHeader extends React.Component {
     return (
       <div
         styleName="header"
+        role="banner"
         ref={(div) => { this.headerRoot = div; }}
         onMouseLeave={() => {
           if (openedMenu) {
@@ -351,7 +352,7 @@ export default class TopcoderHeader extends React.Component {
           <a href={BASE_URL} styleName="logo" aria-label="Topcoder Homepage">
             <LogoTopcoderWithName height={53} width={135} />
           </a>
-          <ul styleName="main-menu" ref={(ul) => { this.mainMenu = ul; }}>
+          <ul styleName="main-menu" role="navigation" ref={(ul) => { this.mainMenu = ul; }}>
             {mainMenu}
           </ul>
           <div styleName="right-menu">
@@ -401,6 +402,7 @@ export default class TopcoderHeader extends React.Component {
           styleName="search-field"
         >
           <input
+            role="search"
             ref={(input) => { this.searchInput = input; }}
             onKeyPress={(event) => {
               if (event.key === 'Enter') {
