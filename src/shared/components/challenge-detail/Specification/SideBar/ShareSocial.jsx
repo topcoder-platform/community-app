58a62d97e7e314f5c2f000d69b5e3c3810846344
r0hit-gupta <rohit1997gupta@gmail.com>
2019-07-25 20:09:34 +0530

    changes for MSFT-186

--- src/shared/components/challenge-detail/Specification/SideBar/ShareSocial.jsx
+++ src/shared/components/challenge-detail/Specification/SideBar/ShareSocial.jsx
@@ -39,7 +39,7 @@ export default class ShareSocial extends React.Component {
           >
             <div styleName="facebook-icon">
               <span>
-f
+Icon Facebook
               </span>
             </div>
           </a>
