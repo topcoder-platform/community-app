7f3c825b358975f58d96a9331618d0cf6a9ee8f8
prakashdurlabhji <prakashseta@gmail.com>
2019-07-27 13:28:40 +0530

    tests fix

--- src/shared/components/challenge-listing/Sidebar/Footer/index.jsx
+++ src/shared/components/challenge-listing/Sidebar/Footer/index.jsx
@@ -14,38 +14,38 @@ export default function Footer({
 }) {
   const goToLink = (url) => {
     window.open(url);
-  }
+  };
   return (
     <div styleName="sidebar-footer">
       {
         hideTcLinksInFooter ? null : (
           <ul>
             <li>
-              <a role="link" onClick={() => goToLink(`${config.URL.BASE}/about`)} aria-label="About" tabIndex="0">
+              <a role="link" onKeyPress={() => goToLink(`${config.URL.BASE}/about`)} onClick={() => goToLink(`${config.URL.BASE}/about`)} aria-label="About" tabIndex="0">
 About
               </a>
 &nbsp;•&nbsp;
             </li>
             <li>
-              <a role="link" onClick={() => goToLink(`${config.URL.HELP}/hc/en-us/articles/219069687-Contact-Support`)} aria-label="Contact" tabIndex="0">
+              <a role="link" onKeyPress={() => goToLink(`${config.URL.HELP}/hc/en-us/articles/219069687-Contact-Support`)} onClick={() => goToLink(`${config.URL.HELP}/hc/en-us/articles/219069687-Contact-Support`)} aria-label="Contact" tabIndex="0">
 Contact
               </a>
 &nbsp;•&nbsp;
             </li>
             <li>
-              <a role="link" onClick={() => goToLink(config.URL.HELP)}  aria-label="Help" tabIndex="0">
+              <a role="link" onKeyPress={() => goToLink(config.URL.HELP)} onClick={() => goToLink(config.URL.HELP)} aria-label="Help" tabIndex="0">
 Help
               </a>
 &nbsp;•&nbsp;
             </li>
             <li>
-              <a role="link" onClick={() => goToLink(`${config.URL.BASE}/community/how-it-works/privacy-policy/`)}  aria-label="Privacy" tabIndex="0">
+              <a role="link" onKeyPress={() => goToLink(`${config.URL.BASE}/community/how-it-works/privacy-policy/`)} onClick={() => goToLink(`${config.URL.BASE}/community/how-it-works/privacy-policy/`)} aria-label="Privacy" tabIndex="0">
 Privacy
               </a>
 &nbsp;•&nbsp;
             </li>
             <li>
-              <a role="link" onClick={() => goToLink(`${config.URL.BASE}/community/how-it-works/terms/`)} aria-label="Terms" tabIndex="0">
+              <a role="link" onKeyPress={() => goToLink(`${config.URL.BASE}/community/how-it-works/terms/`)} onClick={() => goToLink(`${config.URL.BASE}/community/how-it-works/terms/`)} aria-label="Terms" tabIndex="0">
 Terms
               </a>
             </li>
