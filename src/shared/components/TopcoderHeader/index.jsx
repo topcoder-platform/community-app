07cc88e10d9bb11b6503e12428baa726f8e7d473
PrakashDurlabhji <prakashseta@gmail.com>
2019-09-06 00:03:13 +0530

    Update index.jsx

--- src/shared/components/TopcoderHeader/index.jsx
+++ src/shared/components/TopcoderHeader/index.jsx
@@ -315,9 +315,10 @@ export default class TopcoderHeader extends React.Component {
           }}
           onFocus={event => !isMobile && openMenu(userSubMenu, event.target)}
           onBlur={(event) => {
-            if (!isMobile && activeTrigger
-              && 1 + event.pageY < activeTrigger.bottom) closeMenu();
+            if (!isMobile) closeMenu();
           }}
+          role="link"
+          tabIndex={0}
           styleName="user-menu"
         >
           <div
@@ -325,12 +326,10 @@ export default class TopcoderHeader extends React.Component {
               color: getRatingColor(_.get(normalizedProfile, 'maxRating.rating', 0)),
             }}
             styleName="user-menu-handle"
-            role="button"
-            tabIndex={0}
           >
             {normalizedProfile.handle}
           </div>
-          <span role="link" tabIndex={0}>{userAvatar}</span>
+          <span>{userAvatar}</span>
         </div>
       );
     } else {
