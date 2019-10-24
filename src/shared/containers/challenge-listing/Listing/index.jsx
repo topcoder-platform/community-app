f3f52ced5fb5210deacb2596af823ba945571c52
codeMinter <guri.topcoder@gmail.com>
2019-10-24 12:30:54 +0530

    run build

--- src/shared/containers/challenge-listing/Listing/index.jsx
+++ src/shared/containers/challenge-listing/Listing/index.jsx
@@ -72,7 +72,6 @@ export class ListingContainer extends React.Component {
     const script = document.createElement('script');
     script.innerHTML = "(function(t,e,s,o){var n,c,l;t.SMCX=t.SMCX||[],e.getElementById(o)||(n=e.getElementsByTagName(s),c=n[n.length-1],l=e.createElement(s),l.type='text/javascript',l.async=!0,l.id=o,l.src=['https:'===location.protocol?'https://':'http://','widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd6PjYIXGGOw0KVk2yWQQRPhEwpPgmCCgOC_2BvAyG1MDsy.js'].join(''),c.parentNode.insertBefore(l,c))})(window,document,'script','smcx-sdk');";
     document.body.appendChild(script);
-
     this.loadChallenges();
   }
 
