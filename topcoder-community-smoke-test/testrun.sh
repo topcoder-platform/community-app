d775b791239c420ac2a2ffabe531925e8652bea8
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2019-11-29 19:16:40 +0530

    Update testrun.sh

--- topcoder-community-smoke-test/testrun.sh
+++ topcoder-community-smoke-test/testrun.sh
@@ -2,3 +2,9 @@
 #./node_modules/.bin/webdriver-manager start --versions.chrome=75.0.3770.142 --detach
 /node_modules/.bin/webdriver-manager start --detach
 npm run test
+if [ $? -eq 0 ]; then
+  echo "Test case successfully completed"
+else
+  echo "Test case Failed  - $result"
+  exit 1
+fi
