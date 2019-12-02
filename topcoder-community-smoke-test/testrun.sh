4e532a26e0aac6a31f13dcb586eecdade4901578
Deepak Thapa <drasticdpk@gmail.com>
2019-12-02 18:30:51 +0545

    Update  config file setting.

--- topcoder-community-smoke-test/testrun.sh
+++ topcoder-community-smoke-test/testrun.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #./node_modules/.bin/webdriver-manager start --versions.chrome=75.0.3770.142 --detach
 ./node_modules/.bin/webdriver-manager start --detach
-#Set configuration for different environment
-echo "{$SMOKE_CONFIG_PROD}"  | base64 -d > config.json 
 npm run test
 if [ $? -eq 0 ]; then
   echo "Test case successfully completed"
