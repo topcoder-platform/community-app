d9e1377114c8a1ec0f94f60787b247aabbc166a7
Deepak Thapa <drasticdpk@gmail.com>
2020-01-10 19:03:57 +0545

    Smoke Test Configuratoin Update

--- topcoder-community-smoke-test/conf.ts
+++ topcoder-community-smoke-test/conf.ts
@@ -19,8 +19,8 @@ exports.config = {
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
         'browserName': 'chrome',
-        'shardTestFiles': true,
-        'maxInstances': 2,
+        // 'shardTestFiles': true,
+        // 'maxInstances': 2,
         chromeOptions: {
             args: ['--headless', '--disable-gpu', '--window-size=1325x744']
         }
@@ -31,15 +31,15 @@ exports.config = {
 
     specs: [
         '../temp/test-suites/tc-profile.spec.js', 
-        '../temp/test-suites/tc-tools.spec.js', 
+         '../temp/test-suites/tc-tools.spec.js', 
         '../temp/test-suites/tc-account.spec.js', 
         '../temp/test-suites/tc-preferences.spec.js',
 
-        '../temp/test-suites/tc-challenge-listing.spec.js', 
-        '../temp/test-suites/tc-challenge-detail.spec.js', 
-        '../temp/test-suites/tc-dashboard.spec.js', 
-        '../temp/test-suites/tc-footer.spec.js', 
-        '../temp/test-suites/tc-header.spec.js'
+        // '../temp/test-suites/tc-challenge-listing.spec.js', 
+        // '../temp/test-suites/tc-challenge-detail.spec.js', 
+        // '../temp/test-suites/tc-dashboard.spec.js', 
+        // '../temp/test-suites/tc-footer.spec.js', 
+        // '../temp/test-suites/tc-header.spec.js'
     ],
 
     // Options to be passed to Jasmine.
@@ -50,33 +50,24 @@ exports.config = {
     },
 
     onPrepare: () => {
-        return new Promise(function (fulfill, reject) {
-            browser.manage().window().maximize();
-            browser.manage().timeouts().implicitlyWait(5000);
-
-            browser.getCapabilities().then(function (value) {
-                const reportName = value.get('browserName') + '_' + Math.floor(Math.random()*1E16);
-
-                var junitReporter = new reporters.JUnitXmlReporter({
-                    savePath: 'test-results',
-                    consolidateAll: true,
-                    filePrefix: reportName
-                });
-                jasmine.getEnv().addReporter(junitReporter);
-
-                jasmine.getEnv().addReporter(new HtmlReporter({
-                    baseDirectory: 'test-results',
-                    preserveDirectory: true, // Preserve base directory
-                    screenshotsSubfolder: 'screenshots',
-                    jsonsSubfolder: 'jsons', // JSONs Subfolder
-                    takeScreenShotsForSkippedSpecs: true, // Screenshots for skipped test cases
-                    takeScreenShotsOnlyForFailedSpecs: false, // Screenshots only for failed test cases
-                    docTitle: 'Test Automation Execution Report', // Add title for the html report
-                    docName: 'TestReport.html', // Change html report file name
-                    gatherBrowserLogs: true // Store Browser logs
-                }).getJasmine2Reporter());
-                fulfill();
-            })
-        })
+        browser.manage().window().maximize();
+        browser.manage().timeouts().implicitlyWait(5000);
+        var junitReporter = new reporters.JUnitXmlReporter({
+            savePath: 'test-results',
+            consolidateAll: false
+        });
+        jasmine.getEnv().addReporter(junitReporter);
+        // Only for local deployment
+        jasmine.getEnv().addReporter(new HtmlReporter({
+            baseDirectory: 'test-results',
+            preserveDirectory: false, // Preserve base directory
+            screenshotsSubfolder: 'screenshots',
+            jsonsSubfolder: 'jsons', // JSONs Subfolder
+            takeScreenShotsForSkippedSpecs: true, // Screenshots for skipped test cases
+            takeScreenShotsOnlyForFailedSpecs: false, // Screenshots only for failed test cases
+            docTitle: 'Test Automation Execution Report', // Add title for the html report
+            docName: 'TestResult.html', // Change html report file name
+            gatherBrowserLogs: true // Store Browser logs
+        }).getJasmine2Reporter());
     }
 }
