9da4793158a3114d4f4d885fe2e7c4b5e9bade64
LieutenantRoger <gair_2015@hotmail.com>
2020-01-11 13:26:05 +0800

    Fix issue 2719

--- config/default.js
+++ config/default.js
@@ -236,29 +236,32 @@ module.exports = {
     {
       id: 'community', // required for 'Switch to BUSINESS' to work
       title: 'COMMUNITY',
-      secondaryMenuForLoggedInUser: [
+      secondaryMenu: [
         {
           title: 'Dashboard',
           href: '/my-dashboard',
+          logged: true,
         },
         {
           id: 'myprofile',
           title: 'My Profile',
           href: '/members/willFilledByUserName',
+          logged: true,
         },
         {
           title: 'Payments',
           href: 'https://community.topcoder-dev.com/PactsMemberServlet?module=PaymentHistory&full_list=false',
+          logged: true,
         },
-      ],
-      secondaryMenuForGuest: [
         {
           title: 'Overview',
           href: '/community/learn',
+          logged: false,
         },
         {
           title: 'How It Works',
           href: '/thrive/tracks?track=Topcoder',
+          logged: false,
         },
       ],
       subMenu: [
