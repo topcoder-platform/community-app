e81479c901db2b6732401728af008d461db0f1f2
Deepak Thapa <drasticdpk@gmail.com>
2020-02-02 22:00:56 +0545

    Move and Copy update logic

--- topcoder-community-smoke-test/smoketest.sh
+++ topcoder-community-smoke-test/smoketest.sh
@@ -20,15 +20,14 @@ LOGICAL_PATH=$3
 cd topcoder-community-smoke-test
 aws s3 cp s3://tc-platform-${LOGICAL_PATH}/securitymanager/${APPCONFIGFILENAME} .
 track_error $? "Environment setting"
+cp ${APPCONFIGFILENAME} config.json
 if [ $ENV == 'qa' ]; then
-  cp  $cAPPCONFIGFILENAME config-qa.json
+  mv  $cAPPCONFIGFILENAME config-qa.json
 elif [ $ENV == 'dev' ]; then
-   cp  $APPCONFIGFILENAME config-dev.json
+   mv  $APPCONFIGFILENAME config-dev.json
 else
-   cp  $APPCONFIGFILENAME config-prod.json
+   mv  $APPCONFIGFILENAME config-prod.json
 fi
-# for config file
-mv ${APPCONFIGFILENAME} config.json
 docker build -t comm-smoke:latest .
 docker run --shm-size=2g comm-smoke:latest ./testrun.sh ${ENV} -d -p 4444:4444
 track_error $? "Test case Failed"
