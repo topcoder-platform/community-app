c7f35fa361b257cbcdf9f3c3b29f78dea0ce40ac
prakashdurlabhji <prakashseta@gmail.com>
2020-03-12 03:26:20 +0530

    issue 4089 fix

--- src/shared/components/challenge-listing/Filters/ChallengeFilters.jsx
+++ src/shared/components/challenge-listing/Filters/ChallengeFilters.jsx
@@ -46,6 +46,7 @@ export default function ChallengeFilters({
   if (filterState.tags) filterRulesCount += 1;
   if (filterState.subtracks) filterRulesCount += 1;
   if (filterState.endDate || filterState.startDate) filterRulesCount += 1;
+  if (selectedCommunityId !== '') filterRulesCount += 1;
 
   const isTrackOn = track => !filterState.tracks || Boolean(filterState.tracks[track]);
 
