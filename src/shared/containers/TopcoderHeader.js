d8f1fc967c076d4c686b3328cbdf33cbcdfb47e2
Luiz R. Rodrigues <contato@luizrrodrigues.com.br>
2020-03-27 16:28:05 -0300

    #4140: Added check if token expired in Header

--- src/shared/containers/TopcoderHeader.js
+++ src/shared/containers/TopcoderHeader.js
@@ -1,6 +1,8 @@
 /**
  * Container for the standard Topcoder header.
  */
+/* global location */
+/* eslint-disable no-restricted-globals */
 
 import _ from 'lodash';
 import headerActions from 'actions/topcoder_header';
@@ -9,6 +11,29 @@ import { actions } from 'topcoder-react-lib';
 import TopcoderHeader from 'components/Header';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
+import { isTokenExpired } from 'tc-accounts';
+import { config } from 'topcoder-react-utils';
+
+/**
+ * Does nothing if a valid TC API v3 token is passed in; otherwise redirects
+ * user to the TC auth page, with proper return URL.
+ * @param {String} tokenV3
+ * @return {Boolean} `true` if the user is authenticated; `false` otherwise.
+ */
+function authCheck(tokenV3) {
+  if (tokenV3 && !isTokenExpired(tokenV3)) return true;
+
+  /* This implements front-end redirection. Once the server-side rendering of
+    * the Dashboard is in place, this should be updated to work for the server
+    * side rendering as well. */
+  let url = `retUrl=${encodeURIComponent(location.href)}`;
+  url = `${config.URL.AUTH}/member?${url}&utm_source=community-app-main`;
+  location.href = url;
+
+  _.noop(this);
+  return false;
+}
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -37,6 +62,8 @@ function mapDispatchToProps(dispatch) {
 }
 
 function mapStateToProps(state) {
+  authCheck(state.auth.tokenV3);
+
   return {
     ...state.topcoderHeader,
     profile: {
