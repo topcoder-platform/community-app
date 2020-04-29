4b8016abee5c3ecaeb8ad937e8c2d6e1110f81ff
Kiril Kartunov <kiril.kartunov@gmail.com>
2020-04-29 17:25:05 +0300

    Implement #4050

--- src/shared/components/Leaderboard/ChallengeHistoryModal/index.jsx
+++ src/shared/components/Leaderboard/ChallengeHistoryModal/index.jsx
@@ -1,77 +1,147 @@
-import React from 'react';
-import { Modal, PrimaryButton } from 'topcoder-react-ui-kit';
+import React, { Component } from 'react';
+import { Modal } from 'topcoder-react-ui-kit';
 import PT from 'prop-types';
 import LoadingIndicator from 'components/LoadingIndicator';
 import { config } from 'topcoder-react-utils';
+import cn from 'classnames';
+import _ from 'lodash';
 import theme from './styles.scss';
 import PodiumSpot from '../PodiumSpot';
 
+class ChallengeHistoryModal extends Component {
+  constructor(props) {
+    super(props);
 
-function ChallengeHistoryModal({
-  challenges,
-  competitor,
-  onCancel,
-  loading,
-  isCopilot,
-  isAlgo,
-}) {
-  return (
-    <Modal onCancel={onCancel} theme={theme}>
-      <h1>
-        Completed Challenges History
-      </h1>
-      <div styleName="podium-spot-wrapper">
-        <PodiumSpot
-          competitor={competitor}
-          isCopilot={isCopilot}
-          isAlgo={isAlgo}
-          themeName="Default"
-        />
-      </div>
-      <div styleName="head">
-        <div styleName="col-1">
-          Challenge Name
+    this.state = {
+      sortParam: {
+        order: '',
+        field: '',
+      },
+    };
+  }
+
+  render() {
+    const {
+      challenges,
+      competitor,
+      onCancel,
+      loading,
+      isCopilot,
+      isAlgo,
+    } = this.props;
+    const { sortParam } = this.state;
+    const challengesOrdered = _.orderBy(challenges, [sortParam.field], [sortParam.order]);
+
+    return (
+      <Modal onCancel={onCancel} theme={theme}>
+        <h3>
+          Completed Challenges History
+        </h3>
+        <div styleName="podium-spot-wrapper">
+          <PodiumSpot
+            competitor={competitor}
+            isCopilot={isCopilot}
+            isAlgo={isAlgo}
+            themeName="TCO20"
+          />
         </div>
-        {
-          !isCopilot ? (
-            <div styleName="col-2">Placement</div>
-          ) : null
-        }
-        <div styleName="col-3">
-          TCO Points
-        </div>
-      </div>
-      <div styleName="body">
-        {
-          challenges.map(challenge => (
-            <div styleName="row" key={`${challenge.challenge_id}`}>
-              <div styleName="col-1">
-                <a href={`${config.URL.BASE}/challenges/${challenge.challenge_id}/`} styleName="link" target="_blank" rel="noopener noreferrer">
-                  {challenge.challenge_name || challenge.challenge_id}
-                </a>
-              </div>
+        <table styleName="history-table">
+          <thead>
+            <tr>
+              <th>Challenge Name</th>
               {
                 !isCopilot ? (
-                  <div styleName="col-2">{challenge.place}</div>
+                  <th>
+                    <div styleName="header-table-content">
+                      <span>Placement</span>
+                      <button
+                        styleName="sort-container"
+                        onClick={() => {
+                          if (!sortParam.field || sortParam.field !== 'place') {
+                            sortParam.field = 'place';
+                            sortParam.order = 'desc';
+                          } else {
+                            sortParam.order = sortParam.order === 'asc' ? 'desc' : 'asc';
+                          }
+                          this.setState({ sortParam });
+                        }}
+                        type="button"
+                      >
+                        <div styleName={cn('sort-up', {
+                          active: sortParam.field === 'place' && sortParam.order === 'asc',
+                        })}
+                        />
+                        <div styleName={cn('sort-down', {
+                          active: sortParam.field === 'place' && sortParam.order === 'desc',
+                        })}
+                        />
+                      </button>
+                    </div>
+                  </th>
                 ) : null
               }
-              <div styleName="col-3">
-                {challenge.points}
-              </div>
-            </div>
-          ))
+              <th>
+                <div styleName="header-table-content">
+                  <span>TCO Points</span>
+                  <button
+                    styleName="sort-container"
+                    onClick={() => {
+                      if (!sortParam.field || sortParam.field !== 'points') {
+                        sortParam.field = 'points';
+                        sortParam.order = 'desc';
+                      } else {
+                        sortParam.order = sortParam.order === 'asc' ? 'desc' : 'asc';
+                      }
+                      this.setState({ sortParam });
+                    }}
+                    type="button"
+                  >
+                    <div styleName={cn('sort-up', {
+                      active: sortParam.field === 'points' && sortParam.order === 'asc',
+                    })}
+                    />
+                    <div styleName={cn('sort-down', {
+                      active: sortParam.field === 'points' && sortParam.order === 'desc',
+                    })}
+                    />
+                  </button>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              challengesOrdered.map(challenge => (
+                <tr styleName="row" key={`${challenge.challenge_id}`}>
+                  <td styleName="name">
+                    <a href={`${config.URL.BASE}/challenges/${challenge.challenge_id}/`} styleName="link" target="_blank" rel="noopener noreferrer">
+                      {challenge.challenge_name || challenge.challenge_id}
+                    </a>
+                  </td>
+                  {
+                    !isCopilot ? (
+                      <td styleName="placement">{challenge.place}</td>
+                    ) : null
+                  }
+                  <td styleName="points">
+                    {challenge.points}
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        {
+          loading ? <LoadingIndicator /> : null
         }
-      </div>
-      {
-        loading ? <LoadingIndicator /> : null
-      }
-      <div styleName="buttons">
-        <PrimaryButton onClick={onCancel}>
-          Close
-        </PrimaryButton>
-      </div>
-    </Modal>
-  );
+        <div styleName="buttons">
+          <button onClick={onCancel} type="button" styleName="close-btn">
+            Return to page
+          </button>
+        </div>
+      </Modal>
+    );
+  }
 }
 
 const CHALLENGES_TYPE = PT.arrayOf(PT.shape({
