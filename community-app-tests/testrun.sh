09ac39f7cc291bfdc0749ccdefd820274d1a62d6
Sathya <sathya.jayabal@gmail.com>
2020-05-18 15:43:51 +0530

    fix: updated TQ import

--- community-app-tests/testrun.sh
+++ community-app-tests/testrun.sh
@@ -10,6 +10,11 @@ sudo chmod 755 testquality-linux
 ./testquality-linux login ${email} ${password}  --save 
 ./testquality-linux upload_test_run test-results/junitresults-TopcoderLoginPageTests.xml --project_name='POC Automation Framework' --plan_name="'$planName'"
 ./testquality-linux upload_test_run test-results/junitresults-TopcoderToolsPageTests.xml --project_name='POC Automation Framework' --plan_name="'$planName'"
+./testquality-linux upload_test_run test-results/junitresults-TopcoderAccountPageTests.xml --project_name='POC Automation Framework' --plan_name="'$planName'"
+./testquality-linux upload_test_run test-results/junitresults-TopcoderFooterTests.xml --project_name='POC Automation Framework' --plan_name="'$planName'"
+./testquality-linux upload_test_run test-results/junitresults-TopcoderHeaderTests.xml --project_name='POC Automation Framework' --plan_name="'$planName'"
+./testquality-linux upload_test_run test-results/junitresults-TopcoderPreferencesPageTests.xml --project_name='POC Automation Framework' --plan_name="'$planName'"
+./testquality-linux upload_test_run test-results/junitresults-junitresults-TopcoderProfileSettingsPageTests.xml --project_name='POC Automation Framework' --plan_name="'$planName'"
 
 if [ $? -eq 0 ]; then
   echo "Test case successfully completed"
