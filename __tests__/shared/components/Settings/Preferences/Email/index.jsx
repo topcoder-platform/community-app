d50ce4bf074786469a24f5dd6f586b695dffdec4
Kiril Kartunov <kiril.kartunov@gmail.com>
2020-06-05 09:45:14 +0300

    Fixed tests

--- __tests__/shared/components/Settings/Preferences/Email/index.jsx
+++ __tests__/shared/components/Settings/Preferences/Email/index.jsx
@@ -6,6 +6,6 @@ import Email from 'components/Settings/Preferences/Email';
 const rnd = new Renderer();
 
 it('renders email preferences setting page correctly', () => {
-  rnd.render((<Email email={'test@test.com'} preferences={[]}/>));
+  rnd.render((<Email email="test@test.com" preferences={[]} />));
   expect(rnd.getRenderOutput()).toMatchSnapshot();
 });
