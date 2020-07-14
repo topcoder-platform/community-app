feff64e4126c5756e9c9bc930e75e7ab1be4d703
Kiril Kartunov <kiril.kartunov@gmail.com>
2020-07-14 17:12:54 +0300

    Mobile support for #4502

--- src/shared/components/PolicyPages/index.jsx
+++ src/shared/components/PolicyPages/index.jsx
@@ -2,16 +2,17 @@
  * PolicyPages component.
  */
 import _ from 'lodash';
-import React from 'react';
+import React, { useState } from 'react';
 import PT from 'prop-types';
 import Sticky from 'react-stickynode';
 import { config, Link } from 'topcoder-react-utils';
 import cn from 'classnames';
 import ContentBlock from 'components/Contentful/ContentBlock';
 import Error404 from 'components/Error404';
+import MediaQuery from 'react-responsive';
 import './styles.scss';
 
-const menuItems = (policyData, slug) => _.map(policyData, item => (
+const menuItems = (policyDataMenu, slug) => _.map(policyDataMenu, item => (
   <li key={item.slug} styleName={cn({ active: slug === item.slug })}>
     <Link to={`${config.POLICY_PAGES_PATH}/${item.slug}`}>{item.menuLinkText}</Link>
   </li>
@@ -21,6 +22,14 @@ function PolicyPages({
   match,
   policyData,
 }) {
+  const [mNavi, setMNavi] = useState({ policies: false, legal: false });
+
+  function handeSectionClick() {
+    setMNavi({
+      policies: this === 'policies', legal: this === 'legal',
+    });
+  }
+
   let { slug } = match.params;
   const pages = policyData.Policies.concat(policyData.Legal);
   if (!slug) {
@@ -28,17 +37,50 @@ function PolicyPages({
     slug = pages[0].slug;
   }
   const policyPage = _.find(pages, { slug });
-  console.log('PolicyPages', slug, policyData, policyPage, pages[0]);
   return (
     <div styleName="policy-pages-container">
-      <Sticky top=".policy-pages-container">
-        <div styleName="navi">
-          <p styleName="navi-section-title">Policies</p>
-          <ul styleName="navi-section">{menuItems(policyData.Policies, slug)}</ul>
-          <p styleName="navi-section-title2">Legal</p>
-          <ul styleName="navi-section">{menuItems(policyData.Legal, slug)}</ul>
+      <MediaQuery minWidth={769}>
+        <Sticky top=".policy-pages-container">
+          <div styleName="navi">
+            <p styleName="navi-section-title">Policies</p>
+            <ul styleName="navi-section">{menuItems(policyData.Policies, slug)}</ul>
+            <p styleName="navi-section-title2">Legal</p>
+            <ul styleName="navi-section">{menuItems(policyData.Legal, slug)}</ul>
+          </div>
+        </Sticky>
+      </MediaQuery>
+      <MediaQuery maxWidth={768}>
+        <div styleName="mobile-navi">
+          <div
+            role="button"
+            tabIndex="0"
+            styleName={cn('mobile-navi-section-title', { active: mNavi.policies === true })}
+            onClick={handeSectionClick.bind('policies')}
+            onKeyUp={handeSectionClick.bind('policies')}
+          >
+            Policies
+          </div>
+          <ul
+            styleName={cn('mobile-navi-section', { active: mNavi.policies === true })}
+          >
+            {menuItems(policyData.Policies, slug)}
+          </ul>
+          <div
+            role="button"
+            tabIndex="0"
+            styleName={cn('mobile-navi-section-title', { active: mNavi.legal === true })}
+            onClick={handeSectionClick.bind('legal')}
+            onKeyUp={handeSectionClick.bind('legal')}
+          >
+            Legal
+          </div>
+          <ul
+            styleName={cn('mobile-navi-section', { active: mNavi.legal === true })}
+          >
+            {menuItems(policyData.Legal, slug)}
+          </ul>
         </div>
-      </Sticky>
+      </MediaQuery>
       <div styleName="page-content">
         {
           slug && !policyPage ? (
