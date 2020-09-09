599b2363f2c720c324aaaef81a7a0303a600cbb1
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2020-09-09 13:57:26 +0530

    changing auth url

--- config/default.js
+++ config/default.js
@@ -114,7 +114,7 @@ module.exports = {
     COMMUNITY_APP: 'https://community-app.topcoder-dev.com',
 
     ARENA: 'https://arena.topcoder-dev.com',
-    AUTH: 'http://accounts.topcoder-dev.com',
+    AUTH: 'http://accounts-auth0.topcoder-dev.com',
     BASE: 'https://www.topcoder-dev.com',
     HOME: '/my-dashboard',
     BLOG: 'https://www.topcoder-dev.com/blog',
