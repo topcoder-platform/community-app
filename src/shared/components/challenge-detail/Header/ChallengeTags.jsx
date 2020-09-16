ba22b45b6b2f5723387781070e5ad2ede88c0d84
rashmi73 <rashmiseta73@gmail.com>
2020-09-17 02:47:52 +0530

    test fix

--- src/shared/components/challenge-detail/Header/ChallengeTags.jsx
+++ src/shared/components/challenge-detail/Header/ChallengeTags.jsx
@@ -62,10 +62,7 @@ export default function ChallengeTags(props) {
         challengeType
         && (
           <TrackTag
-            onClick={() => {
-                return setImmediate(() => setChallengeListingFilter({ types: [challengeType.id] }))
-              }
-            }
+            onClick={() => setImmediate(() => setChallengeListingFilter({ types: [challengeType.id] }))}
             to={`${challengesUrl}?filter[types][0]=${encodeURIComponent(challengeType.id)}`}
           >
             {challengeType.name}
