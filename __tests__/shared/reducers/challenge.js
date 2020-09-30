630e97cf556394673c0f114a51ea0116ae9b0cd7
Luiz R. Rodrigues <contato@luizrrodrigues.com.br>
2020-09-30 11:30:34 -0300

    Change tc-auth-lib name

--- __tests__/shared/reducers/challenge.js
+++ __tests__/shared/reducers/challenge.js
@@ -11,7 +11,7 @@ import { mock, actions } from 'topcoder-react-lib';
 
 const { mockAction } = mock;
 
-jest.setMock('tc-auth-lib', {
+jest.setMock('@topcoder-platform/tc-auth-lib', {
   decodeToken: () => 'User object',
   isTokenExpired: () => false,
 });
