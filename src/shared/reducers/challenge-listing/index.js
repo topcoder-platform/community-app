b11f0188d14c1cbdd86d43a86ed13fbaa911e736
Cagdas U <cagdasugurlu01@gmail.com>
2020-10-03 01:45:18 +0300

    fix(challenge-listing): date filters will use correct query params
    * The date filters at challenge listing page was using `startDateStart`
    and `endDateEnd` query parameters for `GET /challenges` call.
    * It now uses `registrationStartDateStart` and `submissionEndDateEnd`
    instead, in  compliance with previous implementation.
    
    Addresses topcoder-platform/community-app#4984

--- src/shared/reducers/challenge-listing/index.js
+++ src/shared/reducers/challenge-listing/index.js
@@ -401,7 +401,7 @@ function onSetFilter(state, { payload }) {
    * do it very carefuly (many params are not validated). */
   const filter = _.pickBy(_.pick(
     payload,
-    ['tags', 'types', 'name', 'startDateStart', 'endDateEnd', 'groups'],
+    ['tags', 'types', 'name', 'registrationStartDateStart', 'submissionEndDateEnd', 'groups'],
   ), value => (!_.isArray(value) && value && value !== '') || (_.isArray(value) && value.length > 0));
   // if (_.isPlainObject(filter.tags)) {
   //   filter.tags = _.values(filter.tags);
@@ -409,11 +409,11 @@ function onSetFilter(state, { payload }) {
   // if (_.isPlainObject(filter.subtracks)) {
   //   filter.subtracks = _.values(filter.subtracks);
   // }
-  if (filter.startDateStart && !moment(filter.startDateStart).isValid()) {
-    delete filter.startDateStart;
+  if (filter.registrationStartDateStart && !moment(filter.registrationStartDateStart).isValid()) {
+    delete filter.registrationStartDateStart;
   }
-  if (filter.endDateEnd && !moment(filter.endDateEnd).isValid()) {
-    delete filter.endDateEnd;
+  if (filter.submissionEndDateEnd && !moment(filter.submissionEndDateEnd).isValid()) {
+    delete filter.submissionEndDateEnd;
   }
   // console.log(`aaaaa`);
   // console.log(filter);
@@ -810,8 +810,8 @@ function create(initialState) {
       tags: [],
       types: [],
       groups: [],
-      startDateStart: null,
-      endDateEnd: null,
+      registrationStartDateStart: null,
+      submissionEndDateEnd: null,
     },
 
     selectedCommunityId: 'All',
