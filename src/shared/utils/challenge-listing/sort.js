79e582bac21f02efe2424e8703980ceea1f91e9c
prakashdurlabhji <prakashseta@gmail.com>
2020-10-03 23:40:39 +0530

    minor fix

--- src/shared/utils/challenge-listing/sort.js
+++ src/shared/utils/challenge-listing/sort.js
@@ -11,9 +11,9 @@ export const SORTS = {
   MOST_RECENT_START_DATE: 'startDate',
   // NUM_REGISTRANTS: 'num-registrants',
   // NUM_SUBMISSIONS: 'num-submissions',
-  // PRIZE_HIGH_TO_LOW: 'prize-high-to-low',
-  // TIME_TO_REGISTER: 'time-to-register',
-  // TIME_TO_SUBMIT: 'time-to-submit',
+  PRIZE_HIGH_TO_LOW: 'prize-high-to-low',
+  TIME_TO_REGISTER: 'time-to-register',
+  TIME_TO_SUBMIT: 'time-to-submit',
   TITLE_A_TO_Z: 'name',
   REVIEW_OPPORTUNITIES_TITLE_A_TO_Z: 'review-opportunities-title-a-to-z',
   REVIEW_OPPORTUNITIES_PAYMENT: 'review-opportunities-payment',
@@ -21,10 +21,10 @@ export const SORTS = {
 };
 
 export default {
-  // [SORTS.CURRENT_PHASE]: {
-  //   func: (a, b) => a.status.localeCompare(b.status),
-  //   name: 'Current phase',
-  // },
+  [SORTS.CURRENT_PHASE]: {
+    func: (a, b) => a.status.localeCompare(b.status),
+    name: 'Current phase',
+  },
   [SORTS.MOST_RECENT]: {
     // func: (a, b) => moment(b.registrationStartDate).diff(a.registrationStartDate),
     name: 'Most recent',
@@ -42,43 +42,46 @@ export default {
   //   func: (a, b) => b.numOfSubmissions - a.numOfSubmissions,
   //   name: '# of submissions',
   // },
-  // [SORTS.PRIZE_HIGH_TO_LOW]: {
-  //   func: (a, b) => b.totalPrize - a.totalPrize,
-  //   name: 'Prize high to low',
-  // },
-  // [SORTS.TIME_TO_REGISTER]: {
-  //   func: (a, b) => {
-  //     const aDate = moment(a.registrationEndDate || a.submissionEndTimestamp);
-  //     const bDate = moment(b.registrationEndDate || b.submissionEndTimestamp);
+  [SORTS.PRIZE_HIGH_TO_LOW]: {
+    func: (a, b) => b.totalPrize - a.totalPrize,
+    name: 'Prize high to low',
+    order: 'desc',
+  },
+  [SORTS.TIME_TO_REGISTER]: {
+    func: (a, b) => {
+      const aDate = moment(a.registrationEndDate || a.submissionEndTimestamp);
+      const bDate = moment(b.registrationEndDate || b.submissionEndTimestamp);
 
-  //     if (aDate.isBefore() && bDate.isAfter()) return 1;
-  //     if (aDate.isAfter() && bDate.isBefore()) return -1;
-  //     if (aDate.isBefore() && bDate.isBefore()) return bDate.diff(aDate);
+      if (aDate.isBefore() && bDate.isAfter()) return 1;
+      if (aDate.isAfter() && bDate.isBefore()) return -1;
+      if (aDate.isBefore() && bDate.isBefore()) return bDate.diff(aDate);
 
-  //     return aDate.diff(bDate);
-  //   },
-  //   name: 'Time to register',
-  // },
-  // [SORTS.TIME_TO_SUBMIT]: {
-  //   func: (a, b) => {
-  //     function nextSubEndDate(o) {
-  //       if (o.checkpointSubmissionEndDate && moment(o.checkpointSubmissionEndDate).isAfter()) {
-  //         return moment(o.checkpointSubmissionEndDate);
-  //       }
-  //       return moment(o.submissionEndTimestamp);
-  //     }
+      return aDate.diff(bDate);
+    },
+    name: 'Time to register',
+    order: 'desc',
+  },
+  [SORTS.TIME_TO_SUBMIT]: {
+    func: (a, b) => {
+      function nextSubEndDate(o) {
+        if (o.checkpointSubmissionEndDate && moment(o.checkpointSubmissionEndDate).isAfter()) {
+          return moment(o.checkpointSubmissionEndDate);
+        }
+        return moment(o.submissionEndTimestamp);
+      }
 
-  //     const aDate = nextSubEndDate(a);
-  //     const bDate = nextSubEndDate(b);
+      const aDate = nextSubEndDate(a);
+      const bDate = nextSubEndDate(b);
 
-  //     if (aDate.isBefore() && bDate.isAfter()) return 1;
-  //     if (aDate.isAfter() && bDate.isBefore()) return -1;
-  //     if (aDate.isBefore() && bDate.isBefore()) return bDate.diff(aDate);
+      if (aDate.isBefore() && bDate.isAfter()) return 1;
+      if (aDate.isAfter() && bDate.isBefore()) return -1;
+      if (aDate.isBefore() && bDate.isBefore()) return bDate.diff(aDate);
 
-  //     return aDate.diff(bDate);
-  //   },
-  //   name: 'Time to submit',
-  // },
+      return aDate.diff(bDate);
+    },
+    name: 'Time to submit',
+    order: 'desc',
+  },
   [SORTS.TITLE_A_TO_Z]: {
     // func: (a, b) => a.name.localeCompare(b.name),
     name: 'Title A-Z',
