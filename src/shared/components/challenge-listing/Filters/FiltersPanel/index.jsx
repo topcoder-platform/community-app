ae9933335b2629b3fb39083be67694d5c4738b10
PrakashDurlabhji <prakashseta@gmail.com>
2020-10-15 22:31:09 +0530

    Update index.jsx

--- src/shared/components/challenge-listing/Filters/FiltersPanel/index.jsx
+++ src/shared/components/challenge-listing/Filters/FiltersPanel/index.jsx
@@ -433,6 +433,7 @@ export default function FiltersPanel({
               events: [],
               endDateStart: null,
               startDateEnd: null,
+              status: 'Active',
             });
             selectCommunity(defaultCommunityId);
             setSearchText('');
