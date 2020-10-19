3aecb56dd0e7c7d21c81f44334246a847e492146
gets0ul <gets0ul@gmail.com>
2020-10-20 01:39:57 +0700

    fix: remove unused code

--- src/shared/components/challenge-listing/Filters/FiltersPanel/index.jsx
+++ src/shared/components/challenge-listing/Filters/FiltersPanel/index.jsx
@@ -22,7 +22,6 @@
 /* eslint-disable jsx-a11y/label-has-for */
 
 import _ from 'lodash';
-import { challenge as challengeUtils } from 'topcoder-react-lib';
 import React from 'react';
 import PT from 'prop-types';
 import Select from 'components/Select';
@@ -38,7 +37,6 @@ import DateRangePicker from '../DateRangePicker';
 import style from './style.scss';
 import UiSimpleRemove from '../../Icons/ui-simple-remove.svg';
 
-const Filter = challengeUtils.filter;
 
 export default function FiltersPanel({
   communityFilters,
