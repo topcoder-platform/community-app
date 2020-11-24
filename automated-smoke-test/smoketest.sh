276efe1a2edc6237eb28ecb4b195308c47ea5d0d
Deepak Thapa <drasticdpk@gmail.com>
2020-11-24 16:47:25 +0545

    config folder update.

--- automated-smoke-test/smoketest.sh
+++ automated-smoke-test/smoketest.sh
@@ -20,7 +20,7 @@ LOGICAL_PATH=$2
 cd automated-smoke-test
 aws s3 cp s3://tc-platform-${LOGICAL_PATH}/securitymanager/${APPCONFIGFILENAME} .
 track_error $? "Environment setting"
-cp ${APPCONFIGFILENAME} config.json
+cp ${APPCONFIGFILENAME} config/config.json
 
 docker build -t comm-smoke:latest .
 docker run --shm-size=2g comm-smoke:latest ./testrun.sh -d -p 4444:4444
