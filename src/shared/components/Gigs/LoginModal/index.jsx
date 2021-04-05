6742a5f870c3078080243ca5a5b2f68137e58a25
Kiril Kartunov <kiril.kartunov@gmail.com>
2021-04-05 14:30:42 +0300

    Implement #5448

--- src/shared/components/Gigs/LoginModal/index.jsx
+++ src/shared/components/Gigs/LoginModal/index.jsx
@@ -8,6 +8,8 @@ import PT from 'prop-types';
 import React from 'react';
 import { Modal, PrimaryButton } from 'topcoder-react-ui-kit';
 import { config } from 'topcoder-react-utils';
+import SVG from 'react-inlinesvg';
+import MediaQuery from 'react-responsive';
 import tc from 'components/buttons/themed/tc.scss';
 import modalStyle from './modal.scss';
 
@@ -19,6 +21,13 @@ const buttonThemes = {
   tc,
 };
 
+const blobYellow = 'https://images.ctfassets.net/b5f1djy59z3a/3wYRUnrUj3v765abrGRwlM/b0f9b49b4f49dc163e6913559a19b9e3/blob-yellow.svg';
+const progressBar = 'https://images.ctfassets.net/b5f1djy59z3a/2BX7LOrvVzKEarIJ8boCFm/bba0dd3e0180d2dc355809e6c1954631/progress-bar.svg';
+const progressBarMid = 'https://images.ctfassets.net/b5f1djy59z3a/517ZRt9geweW3QTtzlUqJu/11e33e876426f97e0725ba5fff9755f8/progress-bar-mid.svg';
+const progressBarXS = 'https://images.ctfassets.net/b5f1djy59z3a/6QxH7uVKCngtzBaXDn3Od1/3e0222a1ce773cead3f3a45f291f43a6/progress-bar-mobile.svg';
+const blobPurple = 'https://images.ctfassets.net/b5f1djy59z3a/1ZRCwp1uoShcES16lQmeu/ba084734120ffedebcb92b4e3fa2d667/blob-purple.svg';
+const thinkingFace = 'https://images.ctfassets.net/b5f1djy59z3a/1ARy0M8tnHLo5baDRY1GcB/7d15b488029574a8de7adeeda41d38d9/thinking_icon_-_desktop_and_tablet.png';
+
 function LoginModal({ retUrl, onCancel }) {
   return (
     <Modal
@@ -26,21 +35,36 @@ function LoginModal({ retUrl, onCancel }) {
       onCancel={onCancel}
     >
       <div className={modalStyle.loginRequired}>
-        <h3 className={modalStyle.title}>WARNING</h3>
-        <p className={modalStyle.loginMsg}>You must be a Topcoder member to apply!</p>
+        <SVG src={blobYellow} className={modalStyle.blobYellow} />
+        <h3 className={modalStyle.title}>YAY! You are almost done!</h3>
+        <p className={modalStyle.loginMsg}>
+          Looks like you&apos;re not a Topcoder member yet. Or maybe
+          you&apos;re not logged in? <img src={thinkingFace} alt="face icon" />&nbsp;
+          It&apos;s quick to register and it&apos;s free!
+        </p>
+        <MediaQuery minDeviceWidth={769}>
+          <SVG src={progressBar} className={modalStyle.progressBar} />
+        </MediaQuery>
+        <MediaQuery maxDeviceWidth={768} minDeviceWidth={321}>
+          <SVG src={progressBarMid} className={modalStyle.progressBar} />
+        </MediaQuery>
+        <MediaQuery maxDeviceWidth={320}>
+          <SVG src={progressBarXS} className={modalStyle.progressBar} />
+        </MediaQuery>
         <div className={modalStyle.ctaButtons}>
           <PrimaryButton
             onClick={() => {
-              window.location = `${config.URL.AUTH}/member?retUrl=${encodeURIComponent(retUrl)}`;
+              window.location = `${config.URL.AUTH}/member/registration?retUrl=${encodeURIComponent(retUrl)}&mode=signUp&utm_source=gig_listing`;
             }}
             theme={{
               button: buttonThemes.tc['primary-green-md'],
             }}
           >
-            LOGIN
+            REGISTER NOW
           </PrimaryButton>
         </div>
-        <p className={modalStyle.regTxt}>Not a member? It is free to <a href={`${config.URL.AUTH}/member/registration?retUrl=${encodeURIComponent(retUrl)}&mode=signUp&utm_source=gig_listing`}>register</a>!</p>
+        <p className={modalStyle.regTxt}>Already a member? <a href={`${config.URL.AUTH}/member?retUrl=${encodeURIComponent(retUrl)}`}>Login here</a></p>
+        <SVG src={blobPurple} className={modalStyle.blobPurple} />
       </div>
     </Modal>
   );
