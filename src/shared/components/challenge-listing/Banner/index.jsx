a8f5cf44fbf21e2910e047b5dbfdca86f4f9ae4a
Luiz R. Rodrigues <contato@luizrrodrigues.com.br>
2021-05-03 12:57:50 -0300

    Fix clickable area and typo

--- src/shared/components/challenge-listing/Banner/index.jsx
+++ src/shared/components/challenge-listing/Banner/index.jsx
@@ -9,23 +9,33 @@ const Banner = () => {
   const [isDisplayed, setIsDisplayed] = useState(true);
 
   return isDisplayed && (
-    <div styleName="banner">
+    <a
+      href={`${config.PLATFORM_SITE_URL}/earn/find/challenges`}
+      target="_blank"
+      rel="noopener noreferrer"
+      styleName="banner"
+    >
       <div styleName="content">
         <div styleName="banner-info">
           <BannerInfoIcon />
         </div>
-        <span>Click <a href={`${config.PLATFORM_SITE_URL}/earn/find/challenges`} target="_blank" rel="noopener noreferrer">here</a> if you&quot;d like to visit the new BETA Challenge Listings site.</span>
+        <span>
+          Click here if you&apos;d like to visit the new Beta Challenge Listings site
+        </span>
       </div>
 
       <div
         styleName="banner-close"
-        onClick={() => setIsDisplayed(false)}
+        onClick={(e) => {
+          e.preventDefault();
+          setIsDisplayed(false);
+        }}
         aria-hidden="true"
         role="button"
       >
         <BannerCloseIcon />
       </div>
-    </div>
+    </a>
   );
 };
 
