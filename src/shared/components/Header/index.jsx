720eac8125889c627db128022b73d20df8d48ee9
Dedy Wahyudi <d3dyw4hyud1@gmail.com>
2021-05-28 22:06:37 +0700

    tc logo pride

--- src/shared/components/Header/index.jsx
+++ src/shared/components/Header/index.jsx
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { useState, useEffect } from 'react';
 import PT from 'prop-types';
 import { config } from 'topcoder-react-utils';
-import Logo from 'assets/images/tc-logo.svg';
+import Logo from 'assets/images/tc-logo-pride.svg';
 import { tracking } from '../../actions';
 
 import './style.scss';
