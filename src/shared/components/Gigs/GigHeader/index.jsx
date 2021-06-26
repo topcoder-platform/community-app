0cb00e773809d96612f079e8abd057d1881d13b7
dengjun <dengjun@huobi.com>
2021-06-26 23:29:50 +0800

    ci:修复lint

--- src/shared/components/Gigs/GigHeader/index.jsx
+++ src/shared/components/Gigs/GigHeader/index.jsx
@@ -3,25 +3,23 @@ import { config, Link } from 'topcoder-react-utils';
 import BannerInfoIcon from 'assets/images/banner-info.svg';
 import './style.scss';
 
-const GigHeader = () => {
-  return (
-    <div
-      styleName="gig-header"
-    >
-      <div styleName="content">
-        <div styleName="banner-info">
-          <BannerInfoIcon />
-        </div>
-        <span>
-          You have 3 applied Gigs in the system
-        </span>
+const GigHeader = () => (
+  <div
+    styleName="gig-header"
+  >
+    <div styleName="content">
+      <div styleName="banner-info">
+        <BannerInfoIcon />
       </div>
-
-      <div styleName="row-btn">
-        <Link styleName="primary-white-md" to={`${config.PLATFORM_SITE_URL}/earn/my-gigs`}>CHECK GIG APPLICATION STATUS</Link>
-      </div>
+      <span>
+        You have 3 applied Gigs in the system
+      </span>
     </div>
-  );
-};
+
+    <div styleName="row-btn">
+      <Link styleName="primary-white-md" to={`${config.PLATFORM_SITE_URL}/earn/my-gigs`}>CHECK GIG APPLICATION STATUS</Link>
+    </div>
+  </div>
+);
 
 export default GigHeader;
