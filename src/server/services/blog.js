7aa874c1d6f57752dce04f72484836316cb88574
Rakib Ansary <rakibansary@gmail.com>
2021-09-08 09:44:12 +0600

    feature: fetch feeds from topcoder blog rss feed
    -* replace mock feeds with live feed
    * replace <![CDATA[]]> in feed with empty string to resolve parsing issues

--- src/server/services/blog.js
+++ src/server/services/blog.js
@@ -2,24 +2,31 @@
  * Service that fetches blog rss and parses it
  */
 
-import fs from 'fs';
-import path from 'path';
 import RSSParser from 'rss-parser';
 import { isomorphy } from 'topcoder-react-utils';
 import _ from 'lodash';
+import Axios from 'axios';
 
-const MOCK_DATA_PATH = path.resolve(__dirname, '../../assets/mock-data/mock-blog-feed.rss');
-const mockBlogs = isomorphy.isServerSide()
-  ? new RSSParser().parseString(fs.readFileSync(MOCK_DATA_PATH)) : Promise.resolve();
+const fetchFeed = async (url) => {
+  try {
+    const response = await Axios.get(url);
+    return response.data.replace('<![CDATA[]]>', ''); // <![CDATA[]]> causes parsing failures
+  } catch (err) {
+    return '';
+  }
+};
+
+const fetchBlogs = async () => (isomorphy.isServerSide()
+  ? new RSSParser().parseString(await fetchFeed('https://topcoder.com/blog/feed')) : Promise.resolve());
 
 /**
- * Gets blogs and returns as a list
- * @param {number} limit The number of blogs to return
- * @param {string} category blog category to fetch
- * @return {Promise} Resulting blogs.
- */
+  * Gets blogs and returns as a list
+  * @param {number} limit The number of blogs to return
+  * @param {string} category blog category to fetch
+  * @return {Promise} Resulting blogs.
+  */
 export default async function getBlogs(limit, category = null) {
-  const blogs = await mockBlogs;
+  const blogs = await fetchBlogs();
   const filteredBlogs = category
     ? blogs.items.filter(
       blog => _.get(blog, 'categories', []).map(c => c.trim()).includes(category),
