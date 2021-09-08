e6a1fe0819314f62e0d68709c640d8b07377a3ae
Kiril Kartunov <kiril.kartunov@gmail.com>
2021-09-08 10:27:28 +0300

    fixed rss parse bug

--- src/server/services/blog.js
+++ src/server/services/blog.js
@@ -1,16 +1,10 @@
 /**
  * Service that fetches blog rss and parses it
  */
-
-import fs from 'fs';
-import path from 'path';
 import RSSParser from 'rss-parser';
-import { isomorphy } from 'topcoder-react-utils';
+import { config } from 'topcoder-react-utils';
+import { logger } from 'topcoder-react-lib';
 import _ from 'lodash';
-
-const MOCK_DATA_PATH = path.resolve(__dirname, '../../assets/mock-data/mock-blog-feed.rss');
-const mockBlogs = isomorphy.isServerSide()
-  ? new RSSParser().parseString(fs.readFileSync(MOCK_DATA_PATH)) : Promise.resolve();
 
 /**
  * Gets blogs and returns as a list
@@ -19,12 +13,20 @@ const mockBlogs = isomorphy.isServerSide()
  * @return {Promise} Resulting blogs.
  */
 export default async function getBlogs(limit, category = null) {
-  const blogs = await mockBlogs;
-  const filteredBlogs = category
-    ? blogs.items.filter(
-      blog => _.get(blog, 'categories', []).map(c => c.trim()).includes(category),
-    ) : blogs.items;
-  return filteredBlogs
-    .sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate))
-    .slice(0, limit);
+  try {
+    const feed = await fetch(config.URL.BLOG_FEED);
+    const rss = await feed.text();
+    const feedBlogs = new RSSParser().parseString(rss.replace('<![CDATA[]]>', ''));
+    const blogs = await feedBlogs;
+    const filteredBlogs = category
+      ? blogs.items.filter(
+        blog => _.get(blog, 'categories', []).map(c => c.trim()).includes(category),
+      ) : blogs.items;
+    return filteredBlogs
+      .sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate))
+      .slice(0, limit);
+  } catch (e) {
+    logger.error('getBlogs error', e);
+    return [];
+  }
 }
