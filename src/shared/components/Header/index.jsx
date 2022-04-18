ff232344b15bbde321477204862d144b956da1d6
Kiril Kartunov <kiri4a@gmail.com>
2022-04-18 09:38:57 +0300

    new tc logo

--- src/shared/components/Header/index.jsx
+++ src/shared/components/Header/index.jsx
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { useState, useEffect } from 'react';
 import PT from 'prop-types';
 import { config } from 'topcoder-react-utils';
-import Logo from 'assets/images/tc-logo.svg';
+import Logo from 'assets/images/TC-logo-new.png';
 import { tracking } from '../../actions';
 
 import './style.scss';
