73ddb5fa61252732a45514c399c2d38df428c37d
Kiril Kartunov <kiri4a@gmail.com>
2022-06-13 12:32:24 +0300

    dump action for inspection

--- src/shared/reducers/contentful/index.js
+++ src/shared/reducers/contentful/index.js
@@ -61,7 +61,7 @@ function create(init) {
     const environment = _.get(payload, 'environment') || config.CONTENTFUL.DEFAULT_ENVIRONMENT;
     const res = _.get(newState, `${spaceName}.${environment}`);
     if (error || !res) {
-      logger.log('CMS-related error', error, res, `${spaceName}.${environment}`, newState, payload, config.CONTENTFUL.DEFAULT_SPACE_NAME, config.CONTENTFUL.DEFAULT_ENVIRONMENT);
+      logger.log('CMS-related error', error, action, config.CONTENTFUL.DEFAULT_SPACE_NAME, config.CONTENTFUL.DEFAULT_ENVIRONMENT, res, newState);
       return state;
     }
     const st = state[spaceName][environment];
