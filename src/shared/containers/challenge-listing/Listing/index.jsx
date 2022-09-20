2bd260f2c1242d9e7bd0cf9cd981ae33c1d26216
lunarkid <4476442+dedywahyudi@users.noreply.github.com>
2022-09-20 20:08:22 +0700

    fix: no challenges found for logged in

--- src/shared/containers/challenge-listing/Listing/index.jsx
+++ src/shared/containers/challenge-listing/Listing/index.jsx
@@ -133,8 +133,8 @@ export class ListingContainer extends React.Component {
       dropPastChallenges,
       getPastChallenges,
       filterState,
+      loading,
     } = this.props;
-    const { needLoad } = this.state;
     const oldUserId = _.get(prevProps, 'auth.user.userId');
     const userId = _.get(this.props, 'auth.user.userId');
     const handle = _.get(auth, 'user.handle');
@@ -259,7 +259,7 @@ export class ListingContainer extends React.Component {
     }
     if (filterChanged(filter, prevProps.filter)) {
       this.reloadChallenges();
-      if (needLoad) {
+      if (!loading) {
         // eslint-disable-next-line react/no-did-update-set-state
         this.setState({ needLoad: false });
       }
@@ -714,6 +714,7 @@ ListingContainer.defaultProps = {
   queryBucket: BUCKETS.OPEN_FOR_REGISTRATION,
   meta: {},
   expanding: false,
+  loading: false,
   // isBucketSwitching: false,
   // userChallenges: [],
 };
@@ -813,6 +814,7 @@ ListingContainer.propTypes = {
   // getUserChallenges: PT.func.isRequired,
   setSearchText: PT.func.isRequired,
   filterState: PT.shape().isRequired,
+  loading: PT.bool,
 };
 
 const mapStateToProps = (state, ownProps) => {
@@ -873,6 +875,10 @@ const mapStateToProps = (state, ownProps) => {
     meta: cl.meta,
     // userChallenges: cl.userChallenges,
     filterState: cl.filter,
+    loading: Boolean(cl.loadingActiveChallengesUUID)
+      || Boolean(cl.loadingOpenForRegistrationChallengesUUID)
+      || Boolean(cl.loadingMyChallengesUUID) || Boolean(cl.loadingAllChallengesUUID)
+      || Boolean(cl.loadingPastChallengesUUID) || cl.loadingReviewOpportunitiesUUID,
   };
 };
 
