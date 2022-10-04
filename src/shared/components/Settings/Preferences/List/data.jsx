d0a7a09c152508f4e43a099bc1aba1cdaecc367d
lunarkid <4476442+dedywahyudi@users.noreply.github.com>
2022-10-04 20:53:41 +0700

    fix: hide payment on preferences tab

--- src/shared/components/Settings/Preferences/List/data.jsx
+++ src/shared/components/Settings/Preferences/List/data.jsx
@@ -1,5 +1,4 @@
 import forum from 'assets/images/preferences/forum.svg';
-import payment from 'assets/images/preferences/payment.svg';
 
 import { config } from 'topcoder-react-utils';
 
@@ -11,15 +10,6 @@ const data = [
     description: 'To setup your forum preferences, please click the “Go To Forum” button.',
     link: `${config.URL.FORUMS_VANILLA}/profile/preferences`,
     linkTitle: 'GO TO FORUM',
-  },
-  {
-    id: 'payment',
-    icon: payment,
-    name: 'Payment',
-    description: 'To setup your payments preferences, please click the “Go To Payments” button.',
-    link: `${config.URL.COMMUNITY}/PactsMemberServlet?module=PaymentHistory&full_list=false`,
-    altLink: `${config.URL.THRIVE}/articles/Payment%20Methods`,
-    linkTitle: 'GO TO PAYMENTS',
   },
 ];
 
