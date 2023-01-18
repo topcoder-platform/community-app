7f164c6b68596958ed68c025d0a3ab5ec92358b6
Luiz R. Rodrigues <contato@luizrrodrigues.com.br>
2023-01-18 19:02:37 -0300

    Update Sprig snippet code

--- src/server/index.js
+++ src/server/index.js
@@ -92,14 +92,17 @@ const EXTRA_SCRIPTS = [
   <!-- End of topcoder Zendesk Widget script -->`,
   `<script type="text/javascript">
   (function(l,e,a,p) {
-    if (window.Sprig) return;
     window.Sprig = function(){S._queue.push(arguments)}
     var S = window.Sprig;S.appId = a;S._queue = [];window.UserLeap=S;
     a=l.createElement('script');
     a.async=1;a.src=e+'?id='+S.appId;
     p=l.getElementsByTagName('script')[0];
     p.parentNode.insertBefore(a, p);
-  })(document, 'https://cdn.sprig.com/shim.js', "${config.SPRIG_ENVIRONMENT_ID}");
+  })(document, 'https://cdn.sprig.com/shim.js', '${config.SPRIG_ENVIRONMENT_ID}');
+  // Ensures Sprig does not re-survey the same user and increases tracking accuracy
+  Sprig('setUserId', '[INSERT USER ID]');
+  // Sets visitor's email address to enable email surveys
+  Sprig('setEmail', 'user@test.com');
   </script>`,
 ];
 
