c3fd3a474aac4c9673d030a3f9252bb64ddbd2d2
Justin Gasper <jmgasper@gmail.com>
2023-01-24 17:30:54 +1100

    Add in footer type switching based on url parameter

--- src/shared/components/TopcoderFooter/index.jsx
+++ src/shared/components/TopcoderFooter/index.jsx
@@ -8,6 +8,7 @@ export default function TopcoderFooter() {
   const footerRef = useRef();
   const footerInitialized = useRef(false);
   const [footerId, setFooterId] = useState(0);
+  const urlParams = new URLSearchParams(window.location.href);
 
   useEffect(() => {
     uniqueId += 1;
@@ -20,10 +21,20 @@ export default function TopcoderFooter() {
     }
 
     footerInitialized.current = true;
+    let { fullFooter } = getSubPageConfiguration();
+
+    // If url contains navTool url parameter. Overwrite settings with parameter.
+    if (urlParams.get('navTool')) {
+      if (urlParams.get('navTool') === 'tool') {
+        fullFooter = false;
+      } else {
+        fullFooter = true;
+      }
+    }
 
     tcUniNav('init', `footerNav-${footerId}`, {
       type: 'footer',
-      fullFooter: getSubPageConfiguration().fullFooter,
+      fullFooter,
     });
   }, [footerId]);
 
