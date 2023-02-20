b6e7ae0b4e44bf5f78b0d800db7ce6e20c5ec355
Rakib Ansary <rakibansary@gmail.com>
2023-02-21 00:39:47 +0600

    fix: qa env

---
+++ config/webpack/qa.js
@@ -0,0 +1,36 @@
+const webpackMerge = require('webpack-merge'); // eslint-disable-line import/no-extraneous-dependencies
+
+const path = require('path');
+
+const configFactory = require('topcoder-react-utils/config/webpack/app-production');
+
+const webpack = require('webpack');
+
+const defaultConfig = require('./default');
+
+let publicPath = process.env.CDN_URL;
+if (publicPath) publicPath += '/static-assets';
+else publicPath = '/api/cdn/public/static-assets';
+
+const standardDevelopmentConfig = configFactory({
+  context: path.resolve(__dirname, '../..'),
+  entry: {
+    'loading-indicator-animation': './src/client/loading-indicator-animation',
+    main: './src/client',
+  },
+  keepBuildInfo: Boolean(global.KEEP_BUILD_INFO),
+  publicPath,
+});
+
+const jsxRule = standardDevelopmentConfig.module.rules.find(rule => rule.loader === 'babel-loader');
+jsxRule.exclude = [
+  /node_modules[\\/](?!appirio-tech.*|topcoder|tc-|@topcoder)/,
+  /src[\\/]assets[\\/]fonts/,
+  /src[\\/]assets[\\/]images[\\/]dashboard/,
+];
+
+standardDevelopmentConfig.plugins.push(new webpack.DefinePlugin({
+  PUBLIC_PATH: JSON.stringify(publicPath),
+}));
+
+module.exports = webpackMerge.smart(standardDevelopmentConfig, defaultConfig);
