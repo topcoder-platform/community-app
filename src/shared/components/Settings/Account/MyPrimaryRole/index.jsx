eecc534ad384f1a14f6048bb27538cf3a9ce7cda
Rakib Ansary <rakibansary@topcoder.com>
2023-03-17 16:01:50 +0600

    fix: landing url
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>

--- src/shared/components/Settings/Account/MyPrimaryRole/index.jsx
+++ src/shared/components/Settings/Account/MyPrimaryRole/index.jsx
@@ -30,7 +30,7 @@ const MyPrimaryRole = ({
 
   const AUTH_URL = config.URL.AUTH;
   const handleSignoutClick = () => {
-    window.location.href = `${AUTH_URL}?logout=true&retUrl=${encodeURIComponent(config.URL.COMMUNITY_APP)}`;
+    window.location.href = `${AUTH_URL}?logout=true&retUrl=${encodeURIComponent(config.URL.BASE)}`;
   };
 
   return (
