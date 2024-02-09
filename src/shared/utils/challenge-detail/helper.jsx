ce1c2a9cd9a8a4ede3ca1c74e2168e3af962501b
Justin Gasper <jmgasper@gmail.com>
2024-02-09 15:29:39 +1100

    https://topcoder.atlassian.net/browse/CORE-293

--- src/shared/utils/challenge-detail/helper.jsx
+++ src/shared/utils/challenge-detail/helper.jsx
@@ -247,6 +247,12 @@ export default getEndDate;
 export const formatOrdinals = (n) => {
   let ord = '';
   switch (n) {
+    case 1: ord = '1st';
+      break;
+    case 2: ord = '2nd';
+      break;
+    case 3: ord = '3rd';
+      break;
     case '1': ord = '1st';
       break;
     case '2': ord = '2nd';
