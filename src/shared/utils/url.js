8e86304defa64181d1d2d6c9da4d8f63dcc1d24e
Hentry Martin <hentrymartin.p@gmail.com>
2024-12-16 16:41:55 +0100

    fix: lint

--- src/shared/utils/url.js
+++ src/shared/utils/url.js
@@ -15,7 +15,7 @@ import { BUCKETS } from 'utils/challenge-listing/buckets';
 export function getCurrentUrl() {
   if (isomorphy.isServerSide()) return null;
   const url = window.location.href;
-  
+
   if (typeof url === 'string' && url.startsWith('http')) {
     return url;
   }
