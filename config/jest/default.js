338411096642959e0b984340d056c68c5fb0dc8e
Kiril Kartunov <kiril.kartunov@gmail.com>
2025-12-16 09:41:02 +0200

    jest fix

--- config/jest/default.js
+++ config/jest/default.js
@@ -4,6 +4,11 @@ const nodeConfig = require('config');
 config.transformIgnorePatterns[0] = '/node_modules/(?!appirio-tech|topcoder|tc-|@topcoder)';
 // config.testMatch[0] = '**/__tests__/shared/containers/challenge-listing/FilterPanel.jsx';
 
+config.moduleNameMapper = {
+  '^node:(.*)$': '$1', // Map Node.js core alias to keep Jest 23 resolver happy.
+  ...config.moduleNameMapper,
+};
+
 // Include the directories whose tests has been written to minimize coverage time
 config.collectCoverageFrom = ['src/client/*.{js,jsx}', 'src/server/*.{js,jsx}', 'src/shared/*.{js,jsx}'];
 
